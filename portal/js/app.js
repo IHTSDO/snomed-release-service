17897a4cbbab28ed1953ff8f9f8ef2bfda4d32cd
pgwilliams <pwi@ihtsdo.org>
2014-01-14 10:50:34 +0100

    Moved pre and post conditions into left hand menu

--- portal/js/app.js
+++ portal/js/app.js
@@ -3,6 +3,8 @@ window.App = Ember.Application.create();
 App.Router.map(function() {
 	this.resource('configuration');
 	this.resource('build-input');
+	this.resource('pre-conditions');
+	this.resource('post-conditions');	
 	this.resource('build-trigger');
 	this.resource('build-results');
 });
