3fa5db78f7f9f407ae4128f5a174a8482a76ce6e
Kai Kewley <kaikewley@gmail.com>
2014-01-14 14:27:34 +0000

    Ember navigation - release centre, extension, package.

--- portal/js/app.js
+++ portal/js/app.js
@@ -1,10 +1,74 @@
 window.App = Ember.Application.create();
 
 App.Router.map(function() {
-	this.resource('configuration');
-	this.resource('build-input');
-	this.resource('pre-conditions');
-	this.resource('post-conditions');	
-	this.resource('build-trigger');
-	this.resource('build-results');
+	this.resource('release-centre', { path: '/:releaseCentre_id' }, function() {
+		this.resource('extension', { path: '/:extension_id' }, function() {
+			this.resource('package', { path: '/:package_id'}, function() {
+				this.resource('configuration');
+				this.resource('build-input');
+				this.resource('pre-conditions');
+				this.resource('post-conditions');
+				this.resource('build-trigger');
+				this.resource('build-results');
+			});
+		});
+	});
 });
+
+App.FIXTURES = [
+	{
+		id: 'international_release_centre',
+		name: 'International Release Centre',
+		extensions: [
+			{
+				id: 'australian_pathology_extension',
+				name: 'Australian Pathology Extension',
+				packages: [
+					{
+						id: 'full_package',
+						name: 'Full Package'
+					}
+				]
+			}
+		]
+	}
+];
+
+App.IndexRoute = Ember.Route.extend({
+	model: function() {
+		return {
+			releaseCentres: App.FIXTURES
+		}
+	}
+})
+
+App.ReleaseCentreRoute = Ember.Route.extend({
+	model: function(params) {
+		var releaseCentre = App.FIXTURES.findBy('id', params.releaseCentre_id);
+		return  releaseCentre;
+	}
+})
+
+App.ReleaseCentreIndexRoute = Ember.Route.extend({
+	model: function() {
+		return this.modelFor('release-centre');
+	}
+})
+
+App.ExtensionRoute = Ember.Route.extend({
+	model: function(params) {
+		return this.modelFor('release-centre').extensions.findBy('id', params.extension_id)
+	}
+})
+
+App.ExtensionIndexRoute = Ember.Route.extend({
+	model: function() {
+		return this.modelFor('extension');
+	}
+})
+
+App.PackageRoute = Ember.Route.extend({
+	model: function(params) {
+		return this.modelFor('extension').packages.findBy('id', params.package_id)
+	}
+})
