758ea7964d706b808ce06f949f7b8558f3a9e0d6
Kai Kewley <kaikewley@gmail.com>
2014-01-22 11:08:25 +0000

    Returning Extension businessKey as id.

--- build-service/src/main/java/org/ihtsdo/buildcloud/entity/Extension.java
+++ build-service/src/main/java/org/ihtsdo/buildcloud/entity/Extension.java
@@ -1,6 +1,7 @@
 package org.ihtsdo.buildcloud.entity;
 
 import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.annotate.JsonProperty;
 import org.ihtsdo.buildcloud.helper.EntityHelper;
 
 import javax.persistence.*;
@@ -10,11 +11,13 @@ public class Extension {
 
 	@Id
 	@GeneratedValue
+	@JsonIgnore
 	private Long id;
 
 	private String name;
 
 	@Column(unique = true)
+	@JsonProperty("id")
 	private String businessKey;
 
 	@ManyToOne
