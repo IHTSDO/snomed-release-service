64f0e93080a7968200378dcc7f52dda175c51ed2
Kai Kewley <kaikewley@gmail.com>
2014-01-24 14:07:33 +0000

    Api root controller.

---
+++ api/src/main/java/org/ihtsdo/buildcloud/controller/RootController.java
@@ -0,0 +1,27 @@
+package org.ihtsdo.buildcloud.controller;
+
+import org.ihtsdo.buildcloud.controller.helper.HypermediaGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class RootController {
+
+	@Autowired
+	private HypermediaGenerator hypermediaGenerator;
+
+	private static final String[] ROOT_LINK = {"centres"};
+
+	@RequestMapping("/")
+	@ResponseBody
+	public Map getReleaseCentre(HttpServletRequest request) {
+		return hypermediaGenerator.getEntityHypermedia(new HashMap<String, String>(), request, ROOT_LINK);
+	}
+
+}
