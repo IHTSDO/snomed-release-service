95c675dc2fcaeb2cfc4a4752cc9046e27fb932ab
Kai Kewley <kaikewley@gmail.com>
2014-02-04 11:44:27 +0000

    Can post file to the api as a build input file. File dumped into S3 in a flat structure and record stored in the database. More to do here.

--- data-service/src/main/java/org/ihtsdo/buildcloud/dao/InputFileDAO.java
+++ data-service/src/main/java/org/ihtsdo/buildcloud/dao/InputFileDAO.java
@@ -4,4 +4,6 @@ import org.ihtsdo.buildcloud.entity.InputFile;
 
 public interface InputFileDAO {
 	InputFile find(Long buildId, String packageBusinessKey, String inputFileBusinessKey, String authenticatedId);
+
+	void save(InputFile inputFile);
 }
--- data-service/src/main/java/org/ihtsdo/buildcloud/service/InputFileService.java
+++ data-service/src/main/java/org/ihtsdo/buildcloud/service/InputFileService.java
@@ -2,6 +2,7 @@ package org.ihtsdo.buildcloud.service;
 
 import org.ihtsdo.buildcloud.entity.InputFile;
 
+import java.io.InputStream;
 import java.util.Set;
 
 public interface InputFileService {
@@ -9,4 +10,7 @@ public interface InputFileService {
 	Set<InputFile> findAll(String buildCompositeKey, String packageBusinessKey, String authenticatedId);
 
 	InputFile find(String buildCompositeKey, String packageBusinessKey, String inputFileBusinessKey, String authenticatedId);
+
+	InputFile create(String buildCompositeKey, String packageBusinessKey, String inputFileBusinessKey,
+					 InputStream fileStream, long fileSize, String authenticatedId);
 }
