818bb24b2541ff86eda216173ebde1205e9e3043
Kai Kewley <kaikewley@gmail.com>
2014-02-17 17:33:40 +0100

    Cherry Picking Master commits onto "develop"

--- web/js/app.js
+++ web/js/app.js
@@ -19,6 +19,7 @@ App.Router.map(function() {
 			this.resource('product', { path: '/:product_id' }, function() {
 				this.resource('build', { path: '/:build_id' }, function() {
 					this.resource('package', { path: '/:package_id' }, function() {
+//						this.resource('package-index');
 						this.resource('build-input');
 						this.resource('pre-conditions');
 						this.resource('post-conditions');
