e81d82f0a855ab3228b6f404aa321061400ab6d4
Kai Kewley <kaikewley@gmail.com>
2014-02-26 18:41:05 +0000

    API - Create Product.

--- data-service/src/main/java/org/ihtsdo/buildcloud/service/ProductServiceImpl.java
+++ data-service/src/main/java/org/ihtsdo/buildcloud/service/ProductServiceImpl.java
@@ -3,6 +3,7 @@ package org.ihtsdo.buildcloud.service;
 import org.hibernate.Hibernate;
 import org.ihtsdo.buildcloud.dao.ExtensionDAO;
 import org.ihtsdo.buildcloud.dao.ProductDAO;
+import org.ihtsdo.buildcloud.entity.Extension;
 import org.ihtsdo.buildcloud.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -35,6 +36,15 @@ public class ProductServiceImpl extends EntityServiceImpl<Product> implements Pr
 	@Override
 	public Product find(String releaseCentreBusinessKey, String extensionBusinessKey, String productBusinessKey, String oauthId) {
 		return productDAO.find(releaseCentreBusinessKey, extensionBusinessKey, productBusinessKey, oauthId);
+	}
+
+	@Override
+	public Product create(String releaseCentreBusinessKey, String extensionBusinessKey, String name, String authenticatedId) {
+		Extension extension = extensionDAO.find(releaseCentreBusinessKey, extensionBusinessKey, authenticatedId);
+		Product product = new Product(name);
+		extension.addProduct(product);
+		productDAO.save(product);
+		return product;
 	}
 
 }
