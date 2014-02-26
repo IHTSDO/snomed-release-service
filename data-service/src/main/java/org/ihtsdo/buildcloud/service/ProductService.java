f7d91443e8fc504b23bd8bf6bd57fdb350788808
Kai Kewley <kaikewley@gmail.com>
2014-02-26 18:41:05 +0000

    API - Create Product.

--- data-service/src/main/java/org/ihtsdo/buildcloud/service/ProductService.java
+++ data-service/src/main/java/org/ihtsdo/buildcloud/service/ProductService.java
@@ -1,6 +1,7 @@
 package org.ihtsdo.buildcloud.service;
 
 import org.ihtsdo.buildcloud.entity.Product;
+import org.ihtsdo.buildcloud.entity.ReleaseCentre;
 
 import java.util.List;
 
@@ -9,5 +10,7 @@ public interface ProductService extends EntityService<Product> {
 	List<Product> findAll(String releaseCentreBusinessKey, String extensionBusinessKey, String oauthId);
 
 	Product find(String releaseCentreBusinessKey, String extensionBusinessKey, String productBusinessKey, String oauthId);
+
+	Product create(String releaseCentreBusinessKey, String extensionBusinessKey, String name, String authenticatedId);
 
 }
