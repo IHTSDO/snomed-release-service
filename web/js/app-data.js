103ea96b708a147852bd3d1ac319ac2a8d7d7c8d
Kai Kewley <kaikewley@gmail.com>
2014-02-26 11:21:57 +0000

    Web - Organise ember js.

--- web/js/app-data.js
+++ web/js/app-data.js
@@ -1,17 +1,7 @@
-// Configure REST location
-DS.RESTAdapter.reopen({
-	namespace: 'api/v1'
-});
-
-$.ajaxSetup({
-	beforeSend: function(xhr) {
-		xhr.setRequestHeader('Authorization', 'Basic ' + btoa('test:'));
-	}
-})
-
 App.DSModel = DS.Model.extend(Ember.Validations.Mixin);
 
-// Define business model
+// Models
+
 App.Centre = App.DSModel.extend({
 	name: DS.attr(),
 	shortName: DS.attr(),
@@ -59,72 +49,17 @@ App.InputFile = DS.Model.extend({
 	name: DS.attr()
 });
 
-// REST interface adapter. Adds JSON envelope.
-App.ApplicationSerializer = DS.RESTSerializer.extend({
 
-	// Create json envelope when receiving
-	normalizePayload: function(type, payload) {
+// Configuration
 
-		App.ResolveHypermediaLinks(payload);
-		
-		if (type == 'App.BuildConfig'){
-			var uniqueID = (Math.random()+ "").substring(2) + new Date().getTime();
-			payload['id'] = uniqueID;
-			var payloadJSON = JSON.stringify(payload,null,"\t");
-			payload['configStr'] = payloadJSON;
-		}		
-
-		var o = {};
-		o[type.typeKey + 's'] = payload;
-		return o;
-	},
-
-	// We don't want a json envelope when sending
-	serializeIntoHash: function(hash, type, record, options) {
-		var root = Ember.String.camelize(type.typeKey);
-		var serialized = this.serialize(record, options);
-		$.extend(hash, serialized);
-		console.log('serializeIntoHash');
-		console.log(hash);
-	}
+// Configure REST location
+DS.RESTAdapter.reopen({
+	namespace: 'api/v1'
 });
 
-App.ResolveHypermediaLinks = function(object) {
-	if($.isArray(object)) {
-		$(object).each(function(index, element) {
-			App.ResolveHypermediaLinks(element);
-		})
-	} else {
-		var links = {};
-		var linkFound = false;
-		for (var property in object) {
-			if (object.hasOwnProperty(property)) {
-				var urlPostfixIndex = property.indexOf('_url');
-				if (urlPostfixIndex > 0 && urlPostfixIndex == property.length - 4) {
-					links[property.substring(0, urlPostfixIndex)] = object[property];
-					linkFound = true;
-				}
-			}
-		}
-		if (linkFound) {
-			object.links = links;
-		}
+// API auto authentication
+$.ajaxSetup({
+	beforeSend: function(xhr) {
+		xhr.setRequestHeader('Authorization', 'Basic ' + btoa('test:'));
 	}
-}
-
-// Many Array Reload Extension
-var get = Ember.get;
-DS.PromiseArray.reopen({
-	reloadLinks: function() {
-		var records = get(this, 'content'),
-			store = get(records, 'store'),
-			owner = get(records, 'owner'),
-			type = get(records, 'type'),
-			name = get(records, 'name'),
-			resolver = Ember.RSVP.defer();
-
-		var meta = owner.constructor.metaForProperty(name);
-		var link = owner._data.links[meta.key];
-		store.findHasMany(owner, link, meta, resolver);
-	}
-});
+})
