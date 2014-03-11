899f9f89dd57a6474bb8ef9de477d3fb2954d313
Peter G. Williams <pwi@ihtsdo.org>
2014-03-11 13:52:37 +0000

    Added Spanish builds to test data
    Conflicts:
    	data-service/src/test/resources/org/ihtsdo/buildcloud/service/maven/expected-generated-build-pom.txt
    	model/src/main/java/org/ihtsdo/buildcloud/entity/helper/TestEntityFactory.java
    	model/src/main/java/org/ihtsdo/buildcloud/entity/helper/TestEntityGenerator.java

--- model/src/main/java/org/ihtsdo/buildcloud/entity/helper/TestEntityGenerator.java
+++ model/src/main/java/org/ihtsdo/buildcloud/entity/helper/TestEntityGenerator.java
@@ -13,15 +13,20 @@ public class TestEntityGenerator {
 
 	public static final String [] releaseCentreShortNames = {"International"};
 
-	public static final String [] extensionNames = {"SNOMED CT International Edition"};
+	public static final String [] extensionNames = {"SNOMED CT International Edition",
+													"SNOMED CT Spanish Edition"};
 	
-	public static final String [] productNames = {	"SNOMED CT International Edition",
-													"SNOMED CT Spanish Edition"};		
+	public static final String [][] productNames = {{"SNOMED CT International Edition"},
+													{"SNOMED CT Spanish Edition"} };		
 	
-	public static final String [] buildNames = { "20130731 International Release",
-												 "20140131 International Release - Biannual",
-												 "20140131 International Release - Nightly",
-												 "20140731 International Release - Biannual"};
+	public static final String [][] buildNames = { {"20140731 International Release",
+													"20150131 International Release - Biannual",
+													"20150131 International Release - Nightly",
+													"20150731 International Release - Biannual"},
+												 {	"20141031 Spanish Release",
+													"20150430 Spanish Release - Semestral",
+													"20150430 Spanish Release - Nocturno",
+													"20150430 Spanish Release - Semestral"}};
 	
 	public static final String [] packageNames = {	"RF2 Release",
 													"RF1CompatibilityPackage",
@@ -32,21 +37,23 @@ public class TestEntityGenerator {
 	protected ReleaseCentre createTestReleaseCentre() {
 		
 		ReleaseCentre internationalReleaseCentre = new ReleaseCentre(releaseCentreNames[0], releaseCentreShortNames[0]);
-		Extension extension = new Extension(extensionNames[0]);
-		internationalReleaseCentre.addExtension(extension);
-
-		addProductsToExtension(extension);	
+		addExtensionsToReleaseCentre(internationalReleaseCentre);	
 		return internationalReleaseCentre;
 	}	
 	
-	protected void addProductsToExtension (Extension extension) {
-		for (String productName : productNames) {
-			Product product = new Product (productName);
-			extension.addProduct(product);
-			for (String buildName : buildNames){
-				Build build = new Build(1L, buildName);
-				product.addBuild(build);
-				addPackagesToBuild(build);
+
+	protected void addExtensionsToReleaseCentre (ReleaseCentre releaseCentre) {
+		for (int x=0; x < extensionNames.length; x++){
+			Extension extension = new Extension (extensionNames[x]);
+			releaseCentre.addExtension(extension);
+			for (String productName : productNames[x]) {
+				Product product = new Product (productName);
+				extension.addProduct(product);
+				for (String buildName : buildNames[x]){
+					Build build = new Build(buildName);
+					product.addBuild(build);
+					addPackagesToBuild(build);
+				}
 			}
 		}
 	}
