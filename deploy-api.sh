fd289f0f9feffcbde2123acd1ca11ce2d92db093
Kai Kewley <kaikewley@gmail.com>
2014-04-07 13:17:39 +0100

    Vagrant files organised and commented.

--- deploy-api.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-mvn clean package -Dapple.awt.UIElement='true' && cp api/target/*.war vagrant/tomcat-webapps
--- vagrant/scripts/bootstrap.sh
+++ vagrant/scripts/bootstrap.sh
@@ -15,27 +15,24 @@ service tomcat7 stop
 cp /etc/tomcat7/server.xml /etc/tomcat7/server.xml.org
 sed -i 's/<Connector port="8080" /<Connector port="8081" /' /etc/tomcat7/server.xml
 rm -rf /etc/default/tomcat7
-ln -s /vagrant/vagrant/tomcat7.conf /etc/default/tomcat7
+ln -s /vagrant/vagrant/config/tomcat7.conf /etc/default/tomcat7
 
 # Remove JDK6
 apt-get remove -y openjdk-6-* # Makes JDK7 default
 apt-get autoremove # Remove some unneeded icedtea packages
 
-# Nginx config
+# Nginx config for static webapp
 rm -rf /etc/nginx/sites-available/default
-ln -s /vagrant/vagrant/nginx-site.conf /etc/nginx/sites-available/default
+ln -s /vagrant/vagrant/config/nginx-site.conf /etc/nginx/sites-available/default
 
 # Disable firewall on Ubuntu guest
 ufw disable
 
-# Build API
-cd /vagrant
-mvn clean install -DskipTests=true
-
-# Deploy API
+# Setup Tomcat
 mv /var/lib/tomcat7/webapps /var/lib/tomcat7/_webapps.old
 ln -s /vagrant/vagrant/tomcat-webapps /var/lib/tomcat7/webapps
-cp api/target/api.war vagrant/tomcat-webapps
+
+# Setup API Mock S3
 cp -r /vagrant/vagrant/mock-s3 /tmp
 chown -R tomcat7:tomcat7 /tmp/mock-s3
 
@@ -43,5 +40,15 @@ chown -R tomcat7:tomcat7 /tmp/mock-s3
 service tomcat7 start
 service nginx start
 
-# Start standalone builder process as a background process
-java -jar builder/target/builder.jar &
+# Now deploy
+su vagrant
+
+# Maven Build
+cd /vagrant
+mvn clean install -DskipTests=true
+
+# Deploy API
+cp api/target/api.war vagrant/tomcat-webapps
+
+# Start Builder
+sh /vagrant/builder/scripts/start-builder.sh
