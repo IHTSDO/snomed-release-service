c7c3ba8b1511b1fcea968afb66719a21e6de3508
build-ihtsdo <mch@ihtsdo.org>
2014-06-06 12:24:16 +0100

    Add UUIDTransformation

---
+++ package-processor/src/main/java/org/ihtsdo/buildcloud/packageprocessor/UUIDTransformation.java
@@ -0,0 +1,18 @@
+package org.ihtsdo.buildcloud.packageprocessor;
+
+import java.util.UUID;
+
+/** Replace id in RefSet files with UUID generated.
+ * @author mchu
+ *
+ */
+public class UUIDTransformation implements LineTransformation {
+
+
+	@Override
+	public String[] transformLine(String[] columnValues) {
+		columnValues[0] = UUID.randomUUID().toString();
+		return columnValues;
+	}
+
+}
--- package-processor/src/test/java/org/ihtsdo/buildcloud/packageprocessor/FileTransformationTest.java
+++ package-processor/src/test/java/org/ihtsdo/buildcloud/packageprocessor/FileTransformationTest.java
@@ -68,5 +68,27 @@ public class FileTransformationTest {
 		Assert.assertEquals("First line with new value", "3570b46b-b581-4655-ba2c-9a677a2e880c\t03062014\t0\t900000000000207008\t450990004\t293495006", linesAfter.get(1));
 		Assert.assertEquals("Last line with new value", "c8e26c3c-5f19-41e7-b74b-2ebb889e9e41\t03062014\t0\t900000000000207008\t450990004\t293104008", linesAfter.get(4));
 	}
+	
+	@Test
+	public void testReplaceUUID() throws IOException {
+		fileTransformation.addLineTransformation(new UUIDTransformation() );
+
+		// Assert preconditions
+		List<String> linesBefore = Files.readAllLines(rf2File.toPath(), FileTransformation.UTF_8);
+		Assert.assertEquals(5, linesBefore.size());
+		Assert.assertEquals("Header as expected", "id\teffectiveTime\tactive\tmoduleId\trefSetId\treferencedComponentId", linesBefore.get(0));
+		Assert.assertEquals("First line as expected", "3570b46b-b581-4655-ba2c-9a677a2e880c\t20140131\t1\t900000000000207008\t450990004\t293495006", linesBefore.get(1));
+		Assert.assertEquals("Last line as expected", "c8e26c3c-5f19-41e7-b74b-2ebb889e9e41\t20140131\t1\t900000000000207008\t450990004\t293104008", linesBefore.get(4));
+
+
+		fileTransformation.transformFile(rf2File);
+
+		List<String> linesAfter = Files.readAllLines(rf2File.toPath(), FileTransformation.UTF_8);
+		Assert.assertEquals(5, linesAfter.size());
+		Assert.assertEquals("Header unchanged", "id\teffectiveTime\tactive\tmoduleId\trefSetId\treferencedComponentId", linesAfter.get(0));
+		for( int i =1; i < linesAfter.size() ; i++ ){
+			Assert.assertNotEquals("UUID is changed", linesBefore.get(i), linesAfter.get(i));
+		}
+	}
 
 }
