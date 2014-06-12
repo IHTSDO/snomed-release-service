97e93a53791e132e74b335bb977875f00d74fb16
Peter G. Williams <pwi@ihtsdo.org>
2014-06-12 21:14:25 +0100

    Ready for sprint demo against dev-release server.

--- api-demo/create-simple-refset/create-simple-refset.sh
+++ api-demo/create-simple-refset/create-simple-refset.sh
@@ -7,8 +7,10 @@
 set -e;
 
 # Declare parameters
-api=http://localhost:8080/api/v1
+#api=http://local.ihtsdotools.org/api/v1
+#api=http://localhost:8080/api/v1
 #api="http://uat-release.ihtsdotools.org/api/v1"
+api="http://dev-release.ihtsdotools.org/api/v1"
 buildId="1_20140731_international_release_build"
 packageId="snomed_release_package"
 
