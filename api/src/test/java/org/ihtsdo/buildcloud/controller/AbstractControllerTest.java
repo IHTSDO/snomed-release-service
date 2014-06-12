ede3b448d77f27121066698d0a7f5bde4cfa3dab
Kai Kewley <kaikewley@gmail.com>
2014-06-12 12:46:36 +0100

    AbstractControllerTest rename and super removal.

--- api/src/test/java/org/ihtsdo/buildcloud/controller/AbstractControllerTest.java
+++ api/src/test/java/org/ihtsdo/buildcloud/controller/AbstractControllerTest.java
@@ -6,12 +6,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.ServletException;
@@ -20,8 +18,7 @@ import java.nio.charset.Charset;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"/testDispatcherServletContext.xml"})
 @WebAppConfiguration
-@Transactional
-public abstract class ControllerIntegrationTest extends AbstractJUnit4SpringContextTests {
+public abstract class AbstractControllerTest {
 
 	public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(),
 			MediaType.APPLICATION_JSON.getSubtype(),
