0d72debb29e6b583074671349a7e31f6b271896a
Kai Kewley <kaikewley@gmail.com>
2014-06-26 10:00:30 +0100

    Tidying up integration test code.

--- api/src/test/java/org/ihtsdo/buildcloud/controller/SimpleRefsetTestIntegration.java
+++ api/src/test/java/org/ihtsdo/buildcloud/controller/SimpleRefsetTestIntegration.java
@@ -1,16 +1,6 @@
 package org.ihtsdo.buildcloud.controller;
 
-import static org.hamcrest.Matchers.notNullValue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import javax.servlet.ServletException;
-
+import com.jayway.jsonpath.JsonPath;
 import org.apache.commons.codec.binary.Base64;
 import org.ihtsdo.buildcloud.dao.s3.S3Client;
 import org.ihtsdo.buildcloud.dao.s3.TestS3Client;
@@ -25,32 +15,37 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MvcResult;
 
-import com.jayway.jsonpath.JsonPath;
+import javax.servlet.ServletException;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class SimpleRefsetTestIntegration extends AbstractControllerTest {
 
 	@Autowired
 	private S3Client s3Client;
-	
+
 	public static final String PRODUCT_URL = "/centers/international/extensions/snomed_ct_international_edition/products/nlm_example_refset";
 
 	String basicDigestHeaderValue = "NOT_YET_AUTHENTICATED";
-	
+
 	static final String TEST_PACKAGE = "testpackage";
-	
+
 	@Test
 	public void testMultipleReleases() throws Exception {
-		
-			String buildId = createBuildStructure();
-			
-			boolean isFirstTimeRelease = true;
-			doExecution (buildId,  "2014-01-31", null);
-			
-			isFirstTimeRelease = false;
-			doExecution (buildId,  "2014-07-31", "2014-01-31");
+		String buildId = createBuildStructure();
+
+		// First time release
+		doExecution(buildId, "2014-01-31", null);
+
+		// Second release
+		doExecution(buildId, "2014-07-31", "2014-01-31");
 	}
-	
-	private String createBuildStructure()  throws Exception {
+
+	private String createBuildStructure() throws Exception {
 
 		Assert.assertNotNull(mockMvc);
 
@@ -72,7 +67,7 @@ public class SimpleRefsetTestIntegration extends AbstractControllerTest {
 
 		// Create Build
 		MvcResult createBuildResult = mockMvc.perform(
-			post(PRODUCT_URL +"/builds")
+			post(PRODUCT_URL + "/builds")
 				.header("Authorization", basicDigestHeaderValue)
 				.contentType(MediaType.APPLICATION_JSON)
 				.content("{ \"name\" : \"test-build\" }")
@@ -83,7 +78,7 @@ public class SimpleRefsetTestIntegration extends AbstractControllerTest {
 			.andReturn();
 
 		String buildId = JsonPath.read(createBuildResult.getResponse().getContentAsString(), "$.id");
-		
+
 		// Create Package
 		mockMvc.perform(
 			post("/builds/" + buildId + "/packages")
@@ -94,50 +89,48 @@ public class SimpleRefsetTestIntegration extends AbstractControllerTest {
 			.andDo(print())
 			.andExpect(status().isOk())
 			.andExpect(content().contentType(APPLICATION_JSON_UTF8));
-		
+
 		return buildId;
 	}
 
 	/**
-	 * 
 	 * @param buildId
 	 * @param effectiveDate
 	 * @param previousEffectiveDate if null, then we'll treat this as a first time execution
 	 * @throws Exception
 	 */
-	private void doExecution (String buildId, String effectiveDate, String previousEffectiveDate) throws Exception {
-		
-		boolean isFirstTime = (previousEffectiveDate == null) ? true : false;
-		
+	private void doExecution(String buildId, String effectiveDate, String previousEffectiveDate) throws Exception {
+
+		boolean isFirstTime = previousEffectiveDate == null;
+
 		String packageURL = "/builds/" + buildId + "/packages/" + TEST_PACKAGE;
-		
+
 		//ISO format for date has dashes in it YYYY-MM-DD, strip out to use in filenames
 		String effectiveDateStripped = effectiveDate.replace("-", "");
-		
+
 		// Upload Input File - specific to each run
 		String deltaFileName = "der2_Refset_SimpleDelta_INT_" + effectiveDateStripped + ".txt";
 		MockMultipartFile deltaFile = new MockMultipartFile("file", deltaFileName, "text/plain", getClass().getResourceAsStream("/" + deltaFileName));
 		mockMvc.perform(
-			fileUpload(packageURL +"/inputfiles")
+			fileUpload(packageURL + "/inputfiles")
 				.file(deltaFile)
 				.header("Authorization", basicDigestHeaderValue)
 			)
 			.andDo(print())
 			.andExpect(status().isOk());
-		
+
 		//And if we're doing a subsequent run, we need to delete the input file from the previous run!
 		if (!isFirstTime) {
 			String previousEffectiveDateStripped = previousEffectiveDate.replace("-", "");
-			//String previousDeltaFileName = "der2_Refset_SimpleDelta_INT_" + previousEffectiveDateStripped + "%2Etxt";  //%2E is url friendly hex for a full stop.
 			String previousDeltaFileName = "der2_Refset_SimpleDelta_INT_" + previousEffectiveDateStripped + ".txt";  //%2E is url friendly hex for a full stop.
 			mockMvc.perform(
-					request(HttpMethod.DELETE, packageURL + "/inputfiles/" + previousDeltaFileName)
-						.header("Authorization", basicDigestHeaderValue)
-					)
-					.andDo(print())
-					.andExpect(status().isNoContent());
+				request(HttpMethod.DELETE, packageURL + "/inputfiles/" + previousDeltaFileName)
+					.header("Authorization", basicDigestHeaderValue)
+				)
+				.andDo(print())
+				.andExpect(status().isNoContent());
 		}
-		
+
 		// Upload Manifest - again specific to the release date.   
 		// We're going to give it the same name on upload to ensure it gets overwritten, but the code wipes that directory
 		// on upload anyway.
@@ -145,7 +138,7 @@ public class SimpleRefsetTestIntegration extends AbstractControllerTest {
 		String givenName = "manifest.xml";
 		MockMultipartFile manifestFile = new MockMultipartFile("file", givenName, "text/plain", getClass().getResourceAsStream("/" + manifestFileName));
 		mockMvc.perform(
-			fileUpload(packageURL +"/manifest")
+			fileUpload(packageURL + "/manifest")
 				.file(manifestFile)
 				.header("Authorization", basicDigestHeaderValue)
 			)
@@ -153,7 +146,7 @@ public class SimpleRefsetTestIntegration extends AbstractControllerTest {
 			.andExpect(status().isOk());
 
 		// Set Build effectiveTime
-		String jsonContent = "{ " + jsonPair (BuildService.EFFECTIVE_TIME, effectiveDate) +  " }";
+		String jsonContent = "{ " + jsonPair(BuildService.EFFECTIVE_TIME, effectiveDate) + " }";
 		mockMvc.perform(
 			request(HttpMethod.PATCH, "/builds/" + buildId)
 				.header("Authorization", basicDigestHeaderValue)
@@ -171,20 +164,20 @@ public class SimpleRefsetTestIntegration extends AbstractControllerTest {
 			previousPublishedFile = getPreviousPublishedPackage();
 		}
 
-		jsonContent = 	"{ "
-						+ 		jsonPair ( PackageService.FIRST_TIME_RELEASE, Boolean.toString(isFirstTime))
-						+ 		(isFirstTime ? "" : "," + jsonPair(PackageService.PREVIOUS_PUBLISHED_FULL_FILE, previousPublishedFile))
-						+ " }";
+		jsonContent = "{ "
+				+ jsonPair(PackageService.FIRST_TIME_RELEASE, Boolean.toString(isFirstTime))
+				+ (isFirstTime ? "" : "," + jsonPair(PackageService.PREVIOUS_PUBLISHED_FULL_FILE, previousPublishedFile))
+				+ " }";
 		mockMvc.perform(
-				request(HttpMethod.PATCH, packageURL)
-					.header("Authorization", basicDigestHeaderValue)
-					.contentType(MediaType.APPLICATION_JSON)
-					.content(jsonContent)
-				)
-				.andDo(print())
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(APPLICATION_JSON_UTF8));		
-		
+			request(HttpMethod.PATCH, packageURL)
+				.header("Authorization", basicDigestHeaderValue)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(jsonContent)
+			)
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(APPLICATION_JSON_UTF8));
+
 		// Create Execution
 		MvcResult createExecutionResult = mockMvc.perform(
 			post("/builds/" + buildId + "/executions")
@@ -201,7 +194,7 @@ public class SimpleRefsetTestIntegration extends AbstractControllerTest {
 
 		// Trigger Execution
 		mockMvc.perform(
-			post( executionURL + "/trigger")
+			post(executionURL + "/trigger")
 				.header("Authorization", basicDigestHeaderValue)
 				.contentType(MediaType.APPLICATION_JSON)
 			)
@@ -210,57 +203,56 @@ public class SimpleRefsetTestIntegration extends AbstractControllerTest {
 			.andExpect(content().contentType(APPLICATION_JSON_UTF8));
 
 		//Publish output
-		mockMvc.perform( 
-				post( executionURL + "/output/publish")
+		mockMvc.perform(
+			post(executionURL + "/output/publish")
 				.header("Authorization", basicDigestHeaderValue)
 				.contentType(MediaType.APPLICATION_JSON)
 			)
 			.andDo(print())
 			.andExpect(status().isOk());
 			//.andExpect(content().contentType(APPLICATION_JSON_UTF8));
-		
+
 
 	}
-	
+
 	private String getPreviousPublishedPackage() throws Exception {
-		
+
 		//Recover URL of published things from Product
-		MvcResult productResult = mockMvc.perform( 
-				post(PRODUCT_URL)
+		MvcResult productResult = mockMvc.perform(
+			post(PRODUCT_URL)
 				.header("Authorization", basicDigestHeaderValue)
 				.contentType(MediaType.APPLICATION_JSON)
 			)
 			.andDo(print())
 			.andExpect(status().isOk())
-			.andExpect(content().contentType(APPLICATION_JSON_UTF8)
-			).andReturn();
+			.andExpect(content().contentType(APPLICATION_JSON_UTF8))
+			.andReturn();
+
 		String publishedURL = JsonPath.read(productResult.getResponse().getContentAsString(), "$.published_url");
 		String expectedURL = "http://localhost:80/centers/international/extensions/snomed_ct_international_edition/products/nlm_example_refset/published";
 
 		Assert.assertEquals(expectedURL, publishedURL);
-		
+
 		//Recover list of published packages
-		MvcResult publishedResult = mockMvc.perform( 
-				post( publishedURL )
+		MvcResult publishedResult = mockMvc.perform(
+			post(publishedURL)
 				.header("Authorization", basicDigestHeaderValue)
 				.contentType(MediaType.APPLICATION_JSON)
 			)
 			.andDo(print())
 			.andExpect(status().isOk())
-			.andExpect(content().contentType(APPLICATION_JSON_UTF8)
-			).andReturn();
-		String publishedFile = JsonPath.read(publishedResult.getResponse().getContentAsString(), "$.publishedPackages[0]");
-		return publishedFile;
+			.andExpect(content().contentType(APPLICATION_JSON_UTF8))
+			.andReturn();
+
+		return JsonPath.read(publishedResult.getResponse().getContentAsString(), "$.publishedPackages[0]");
 	}
-	
+
 	/*
 	 * @return a string formatted for use as a JSON key/value pair eg 	"\"effectiveTime\" : \""+ effectiveDate + "\","
 	 * with a trailing comma just in case you want more than one and json is OK with that if there's only one
 	 */
-	private String jsonPair (String key, String value) {
-		
+	private String jsonPair(String key, String value) {
 		return "  \"" + key + "\" : \"" + value + "\" ";
-		
 	}
 
 	@Override
@@ -269,10 +261,5 @@ public class SimpleRefsetTestIntegration extends AbstractControllerTest {
 		super.setup();
 		((TestS3Client) s3Client).deleteBuckets();
 	}
-//
-//	@After
-//	public void tearDown() {
-//		((TestS3Client) s3Client).deleteBuckets();
-//	}
 
 }
