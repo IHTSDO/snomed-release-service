254b0ec5719131141e4076690eb9ea7a22857d01
Peter G. Williams <pwi@ihtsdo.org>
2014-06-27 14:27:28 +0100

    Client script now calling delete to remove previous delta files

--- api-demo/create-refsets/run_complex_first_release.sh
+++ api-demo/create-refsets/run_complex_first_release.sh
@@ -7,7 +7,6 @@ set -e;
 
 # Declare run specific parameters
 inputFilesDir="complex-first"
-unwantedInputFilesDir="simple-subsequent"
 manifestFileName="manifest_complex_20140131.xml"
 effectiveDate="2014-01-31"
 isFirstTime=true
