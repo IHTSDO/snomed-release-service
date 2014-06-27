9db3c9de9ad38b71a0a92486918a53a1821b2935
Peter G. Williams <pwi@ihtsdo.org>
2014-06-27 11:53:35 +0100

    Adding Epic 3 client demo.  Needs delta files and changes to manifest.

---
+++ api-demo/create-refsets/run_complex_first_release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Sets up variables to be consumed by create-refsets
+
+# Stop on error
+set -e;
+
+# Declare run specific parameters
+inputFilesDir="complex-first"
+unwantedInputFilesDir="complex-subsequent"
+manifestFileName="manifest_complex_20140131.xml"
+effectiveDate="2014-01-31"
+isFirstTime=true
+firstTimeStr="true"
+
+#Pass control to our busy worker bee
+calling_program=`basename $0`
+source create-refsets.sh
