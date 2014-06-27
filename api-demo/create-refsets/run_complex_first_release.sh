1d874307c676dda43a0e3eba59f89c334799cc0c
Peter G. Williams <pwi@ihtsdo.org>
2014-06-27 12:25:04 +0100

    Adding data and matching manifest for EPIC2 Demo

--- api-demo/create-refsets/run_complex_first_release.sh
+++ api-demo/create-refsets/run_complex_first_release.sh
@@ -7,7 +7,7 @@ set -e;
 
 # Declare run specific parameters
 inputFilesDir="complex-first"
-unwantedInputFilesDir="complex-subsequent"
+unwantedInputFilesDir="simple-subsequent"
 manifestFileName="manifest_complex_20140131.xml"
 effectiveDate="2014-01-31"
 isFirstTime=true
