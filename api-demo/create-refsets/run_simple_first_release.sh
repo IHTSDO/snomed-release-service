8f8113938f636a0c740f513bd497c8f6e6a16303
Peter G. Williams <pwi@ihtsdo.org>
2014-06-27 11:47:28 +0100

    Refactored client script to be ready to handle more epics

---
+++ api-demo/create-refsets/run_simple_first_release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Sets up variables to be consumed by create-refsets
+
+# Stop on error
+set -e;
+
+# Declare run specific parameters
+inputFilesDir="simple-first"
+unwantedInputFilesDir="simple-subsequent"
+manifestFileName="manifest_simple_20140131.xml"
+effectiveDate="2014-01-31"
+isFirstTime=true
+firstTimeStr="true"
+
+#Pass control to our busy worker bee
+calling_program=`basename $0`
+source create-refsets.sh
