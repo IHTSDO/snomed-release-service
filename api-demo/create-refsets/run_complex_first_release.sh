8ba4097f528de3369bd4fb1a0ca4db73d5534ab9
Kai Kewley <kaikewley@gmail.com>
2014-06-30 12:44:21 +0100

    Script client - better output directory naming.

--- api-demo/create-refsets/run_complex_first_release.sh
+++ api-demo/create-refsets/run_complex_first_release.sh
@@ -6,7 +6,7 @@
 set -e;
 
 # Declare run specific parameters
-inputFilesDir="complex-first"
+executionName="complex-first" # used to find input-files and name output
 manifestFileName="manifest_complex_20140131.xml"
 effectiveDate="2014-01-31"
 isFirstTime=true
