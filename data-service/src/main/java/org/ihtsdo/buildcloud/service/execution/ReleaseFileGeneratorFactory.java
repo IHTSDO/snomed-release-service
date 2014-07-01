4623e399791bb637beb2e82687189cf4d88f6b62
Kai Kewley <kaikewley@gmail.com>
2014-07-01 13:17:24 +0100

    Code formatting.

--- data-service/src/main/java/org/ihtsdo/buildcloud/service/execution/ReleaseFileGeneratorFactory.java
+++ data-service/src/main/java/org/ihtsdo/buildcloud/service/execution/ReleaseFileGeneratorFactory.java
@@ -7,12 +7,11 @@ import org.ihtsdo.buildcloud.entity.Package;
 public class ReleaseFileGeneratorFactory {
 
 	public ReleaseFileGenerator createReleaseFileGenerator(Execution execution, Package pkg, ExecutionDAO dao) {
-	    
-	    if (pkg.isFirstTimeRelease()) {
-		return new FirstReleaseFileGenerator(execution, pkg, dao);
-	    } else {
-		return new SubsequentReleaseFileGenerator(execution, pkg, dao);
-	    }
+		if (pkg.isFirstTimeRelease()) {
+			return new FirstReleaseFileGenerator(execution, pkg, dao);
+		} else {
+			return new SubsequentReleaseFileGenerator(execution, pkg, dao);
+		}
 	}
 
 }
