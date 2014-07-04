eb98078d84b4b836da6ea46092fe351ec06e8d89
Kai Kewley <kaikewley@gmail.com>
2014-07-04 10:02:34 +0100

    Bug: Integration test build clash. Helper now uses build id returned from from creation call.

--- api/src/test/java/org/ihtsdo/buildcloud/controller/helper/IntegrationTestHelper.java
+++ api/src/test/java/org/ihtsdo/buildcloud/controller/helper/IntegrationTestHelper.java
@@ -37,11 +37,10 @@ public class IntegrationTestHelper {
 	public static final String PRODUCT_URL = "/centers/international/extensions/snomed_ct_international_edition/products/nlm_example_refset";
 	public static final String TEST_PACKAGE = "testpackage";
 	public static final String BUILD_NAME = "test-build";
-	public static final String BUILD_ID = "10_testbuild";
-	public static final String PACKAGE_URL = "/builds/" + BUILD_ID + "/packages/" + TEST_PACKAGE;
 
 	private MockMvc mockMvc;
 	private String basicDigestHeaderValue;
+	private String buildId;
 
 	public IntegrationTestHelper(MockMvc mockMvc) {
 		this.mockMvc = mockMvc;
@@ -66,7 +65,7 @@ public class IntegrationTestHelper {
 
 	public void createTestBuildStructure() throws Exception {
 		// Create Build
-		mockMvc.perform(
+		MvcResult createBuildResult = mockMvc.perform(
 				post(PRODUCT_URL + "/builds")
 						.header("Authorization", getBasicDigestHeaderValue())
 						.contentType(MediaType.APPLICATION_JSON)
@@ -77,9 +76,14 @@ public class IntegrationTestHelper {
 				.andExpect(content().contentType(AbstractControllerTest.APPLICATION_JSON_UTF8))
 				.andReturn();
 
+		String createBuildResponseString = createBuildResult.getResponse().getContentAsString();
+		int idStartIndex = createBuildResponseString.indexOf("\"id\" :") + 8;
+		int idEndIndex = createBuildResponseString.indexOf("\"", idStartIndex);
+		this.buildId = createBuildResponseString.substring(idStartIndex, idEndIndex);
+
 		// Create Package
 		mockMvc.perform(
-				post("/builds/" + BUILD_ID + "/packages")
+				post("/builds/" + buildId + "/packages")
 						.header("Authorization", getBasicDigestHeaderValue())
 						.contentType(MediaType.APPLICATION_JSON)
 						.content("{ \"name\" : \"" + TEST_PACKAGE + "\" }")
@@ -92,7 +96,7 @@ public class IntegrationTestHelper {
 	public void uploadDeltaInputFile(String deltaFileName, Class classpathResourceOwner) throws Exception {
 		MockMultipartFile deltaFile = new MockMultipartFile("file", deltaFileName, "text/plain", classpathResourceOwner.getResourceAsStream(deltaFileName));
 		mockMvc.perform(
-				fileUpload(PACKAGE_URL + "/inputfiles")
+				fileUpload(getPackageUrl() + "/inputfiles")
 						.file(deltaFile)
 						.header("Authorization", getBasicDigestHeaderValue())
 		)
@@ -102,7 +106,7 @@ public class IntegrationTestHelper {
 
 	public void deletePreviousTxtInputFiles() throws Exception {
 		mockMvc.perform(
-				request(HttpMethod.DELETE, PACKAGE_URL + "/inputfiles/*.txt")
+				request(HttpMethod.DELETE, getPackageUrl() + "/inputfiles/*.txt")
 						.header("Authorization", getBasicDigestHeaderValue())
 		)
 				.andDo(print())
@@ -112,7 +116,7 @@ public class IntegrationTestHelper {
 	public void uploadManifest(String manifestFileName, Class classpathResourceOwner) throws Exception {
 		MockMultipartFile manifestFile = new MockMultipartFile("file", manifestFileName, "text/plain", classpathResourceOwner.getResourceAsStream(manifestFileName));
 		mockMvc.perform(
-				fileUpload(PACKAGE_URL + "/manifest")
+				fileUpload(getPackageUrl() + "/manifest")
 						.file(manifestFile)
 						.header("Authorization", getBasicDigestHeaderValue())
 		)
@@ -123,7 +127,7 @@ public class IntegrationTestHelper {
 	public void setEffectiveTime(String effectiveDate) throws Exception {
 		String jsonContent = "{ " + jsonPair(BuildService.EFFECTIVE_TIME, getEffectiveDateWithSeparators(effectiveDate)) + " }";
 		mockMvc.perform(
-				request(HttpMethod.PATCH, "/builds/" + BUILD_ID)
+				request(HttpMethod.PATCH, "/builds/" + buildId)
 						.header("Authorization", getBasicDigestHeaderValue())
 						.contentType(MediaType.APPLICATION_JSON)
 						.content(jsonContent)
@@ -148,7 +152,7 @@ public class IntegrationTestHelper {
 	public void setFirstTimeRelease(boolean isFirstTime) throws Exception {
 		String jsonContent = "{ " + jsonPair(PackageService.FIRST_TIME_RELEASE, Boolean.toString(isFirstTime)) + " }";
 		mockMvc.perform(
-				request(HttpMethod.PATCH, PACKAGE_URL)
+				request(HttpMethod.PATCH, getPackageUrl())
 						.header("Authorization", getBasicDigestHeaderValue())
 						.contentType(MediaType.APPLICATION_JSON)
 						.content(jsonContent)
@@ -161,7 +165,7 @@ public class IntegrationTestHelper {
 	public void setPreviousPublishedPackage(String previousPublishedFile) throws Exception {
 		String jsonContent = "{ " + jsonPair(PackageService.PREVIOUS_PUBLISHED_PACKAGE, previousPublishedFile) + " }";
 		mockMvc.perform(
-				request(HttpMethod.PATCH, PACKAGE_URL)
+				request(HttpMethod.PATCH, getPackageUrl())
 						.header("Authorization", getBasicDigestHeaderValue())
 						.contentType(MediaType.APPLICATION_JSON)
 						.content(jsonContent)
@@ -174,7 +178,7 @@ public class IntegrationTestHelper {
 
 	public void setReadmeHeader(String readmeHeader) throws Exception {
 		mockMvc.perform(
-				request(HttpMethod.PATCH, PACKAGE_URL)
+				request(HttpMethod.PATCH, getPackageUrl())
 						.header("Authorization", getBasicDigestHeaderValue())
 						.contentType(MediaType.APPLICATION_JSON)
 						.content("{ \"readmeHeader\" : \"" + readmeHeader + "\" }")
@@ -186,7 +190,7 @@ public class IntegrationTestHelper {
 
 	public String createExecution() throws Exception {
 		MvcResult createExecutionResult = mockMvc.perform(
-				post("/builds/" + BUILD_ID + "/executions")
+				post("/builds/" + buildId + "/executions")
 						.header("Authorization", getBasicDigestHeaderValue())
 						.contentType(MediaType.APPLICATION_JSON)
 		)
@@ -196,7 +200,7 @@ public class IntegrationTestHelper {
 				.andReturn();
 
 		String executionId = JsonPath.read(createExecutionResult.getResponse().getContentAsString(), "$.id");
-		return "/builds/" + BUILD_ID + "/executions/" + executionId;
+		return "/builds/" + buildId + "/executions/" + executionId;
 	}
 
 	public void triggerExecution(String executionURL) throws Exception {
@@ -334,4 +338,8 @@ public class IntegrationTestHelper {
 		return basicDigestHeaderValue;
 	}
 
+	public String getPackageUrl() {
+		return "/builds/" + buildId + "/packages/" + TEST_PACKAGE;
+	}
+	
 }
