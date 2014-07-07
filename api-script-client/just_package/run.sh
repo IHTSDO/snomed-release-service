9704fdc122c0c230aaa426a996b55f36e6da9b82
Peter G. Williams <pwi@ihtsdo.org>
2014-07-07 12:34:12 +0100

    Hotfix to add runner script and tmp directory placeholder.

---
+++ api-script-client/just_package/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e; # Stop on error
+
+# Declare run specific parameters
+effectiveDate="2014-01-31"
+isFirstTime=true
+justPackage=true
+
+# Call api_client
+source ../api_client.sh
