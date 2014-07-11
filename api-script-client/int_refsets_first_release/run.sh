2503375490809b2477ea8d70110e29efaeca651d
Peter G. Williams <pwi@ihtsdo.org>
2014-07-11 16:18:08 +0100

    Rename to reflect that this dataset is international release refsets.

--- api-script-client/int_refsets_first_release/run.sh
+++ api-script-client/int_refsets_first_release/run.sh
@@ -5,7 +5,7 @@ set -e; # Stop on error
 effectiveDate="2014-01-31"
 readmeEndDate="2014"
 isFirstTime=true
-externalDataLocation="multiple-first"
+externalDataLocation="int_refsets_first"
 
 # Call api_client
 source ../api_client.sh
