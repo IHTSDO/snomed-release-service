396c735756259e309373f78a75cf8b652fb92e57
Kai Kewley <kaikewley@gmail.com>
2014-07-15 14:07:44 +0100

    Client script: find or create execution, product, build and package.

--- api-script-client/complex_subsequent/run.sh
+++ api-script-client/complex_subsequent/run.sh
@@ -2,11 +2,12 @@
 set -e; # Stop on error
 
 # Declare run specific parameters
+extensionName="Examples"
+productName="Complex Refset"
 effectiveDate="2014-07-31"
 readmeEndDate="2014"
 isFirstTime=false
 previousPublishedPackageName="ExampleComplexRefset_Release_INT_20140131.zip"
-buildName="ComplexRelease"
 
 # Call api_client
 source ../api_client.sh
--- api-script-client/just_package/run.sh
+++ api-script-client/just_package/run.sh
@@ -2,11 +2,12 @@
 set -e; # Stop on error
 
 # Declare run specific parameters
+extensionName="Examples"
+productName="Just Package"
 effectiveDate="2014-01-31"
 readmeEndDate="2014"
 isFirstTime=true
 justPackage=true
-buildName="JustPackageRelease"
 
 # Call api_client
 source ../api_client.sh
