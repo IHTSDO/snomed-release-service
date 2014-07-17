fb7956d60e81f778a42c7525f43df53c1c8c09b5
Peter G. Williams <pwi@ihtsdo.org>
2014-07-17 16:09:46 +0100

    Add support for additional information when detecting entity already exists.

--- data-service/src/main/java/org/ihtsdo/buildcloud/service/exception/EntityAlreadyExistsException.java
+++ data-service/src/main/java/org/ihtsdo/buildcloud/service/exception/EntityAlreadyExistsException.java
@@ -2,7 +2,16 @@ package org.ihtsdo.buildcloud.service.exception;
 
 public class EntityAlreadyExistsException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public EntityAlreadyExistsException() {
+		super("Entity already exists.");
+	}
+	
+	public EntityAlreadyExistsException(String string) {
 		super("Entity already exists.");
 	}
 }
