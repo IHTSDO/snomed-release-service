552c3db0ee09f96ebda23980d28b71497a03754e
Peter G. Williams <pwi@ihtsdo.org>
2014-07-17 16:14:41 +0100

    Put test into it's own package and set only files available in manifest.

--- api-script-client/int_refsets_subsequent_release/run.sh
+++ api-script-client/int_refsets_subsequent_release/run.sh
@@ -5,9 +5,11 @@ set -e; # Stop on error
 effectiveDate="2014-07-31"
 readmeEndDate="2014"
 isFirstTime=false
+extensionName="SNOMED CT International Edition"
 externalDataLocation="int_refsets_subsequent"
-previousPublishedPackageName="Example_Refsets_Release_20140131.zip"
-buildName="IntRefsetsRelease"
+previousPublishedPackageName="SnomedCT_Release_INT_20140131.zip"
+productName="Int Refsets Release"
+buildName="Int Refsets Release"
 
 # Call api_client
 source ../api_client.sh
