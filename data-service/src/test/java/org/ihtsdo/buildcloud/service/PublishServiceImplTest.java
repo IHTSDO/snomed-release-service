308423c55160d8b63d0cdd1c047ad53538856f6d
Kai Kewley <kaikewley@gmail.com>
2014-07-29 10:20:28 +0100

    Reformat test code.

--- data-service/src/test/java/org/ihtsdo/buildcloud/service/PublishServiceImplTest.java
+++ data-service/src/test/java/org/ihtsdo/buildcloud/service/PublishServiceImplTest.java
@@ -23,111 +23,117 @@ import org.junit.runner.RunWith;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+
 @RunWith(JMockit.class)
 public class PublishServiceImplTest {
-    
-    private static final String PATH_SEPARATOR = "/";
-    private static final String TEST_ZIP = "test.zip";
-    private static final String EXECUTION_OUTPUT = "/execution/output/";
-    private static final String PUBLISHED_BUCKET_NAME = "publishedBucketName";
-    private static final String EXECUTION_BUCKET_NAME = "executionBucketName";
-    private PublishServiceImpl publishServiceImpl;
-    @Mocked private S3Client s3Client;
-    @Mocked private S3ClientHelper s3ClientHelper;
-    @Mocked  ExecutionS3PathHelper s3PathHelper;
 
-    @Before
-    public void setUp(){
-	publishServiceImpl = new PublishServiceImpl(EXECUTION_BUCKET_NAME, PUBLISHED_BUCKET_NAME, s3Client, s3ClientHelper);
-	Deencapsulation.setField(publishServiceImpl, s3PathHelper);
-	//use Deencapsulation in JMockit or use
-	//ReflectionTestUtils.setField(publishServiceImpl, "executionS3PathHelper", s3PathHelper);
-    }
-    
-    @Test
-    public void testPublishExecutionPackage( @Injectable final Execution execution, @Injectable final Package pk) throws IOException {
-	final Build build = createDummyBuild();
-	new Expectations() { {
-	    s3PathHelper.getExecutionOutputFilesPath(execution, anyString);
-	    returns( new StringBuffer(EXECUTION_OUTPUT));
-	    
-	    s3Client.listObjects(EXECUTION_BUCKET_NAME, anyString);
-	    ObjectListing ol = createDummyObjectListing(EXECUTION_OUTPUT + TEST_ZIP);
-	    returns(ol);
-	    
-	    s3PathHelper.getExecutionOutputFilePath(execution, anyString, anyString);
-	    String fileToBePublished = EXECUTION_OUTPUT + "text.zip";
-	    returns( fileToBePublished);
-	    
-	    execution.getBuild();
-	    returns(build);
+	private static final String PATH_SEPARATOR = "/";
+	private static final String TEST_ZIP = "test.zip";
+	private static final String EXECUTION_OUTPUT = "/execution/output/";
+	private static final String PUBLISHED_BUCKET_NAME = "publishedBucketName";
+	private static final String EXECUTION_BUCKET_NAME = "executionBucketName";
+	private PublishServiceImpl publishServiceImpl;
+	@Mocked
+	private S3Client s3Client;
+	@Mocked
+	private S3ClientHelper s3ClientHelper;
+	@Mocked
+	ExecutionS3PathHelper s3PathHelper;
 
-		String zipKey = getExpectedPublishFileDir(build.getProduct()).toLowerCase() + TEST_ZIP;
-		s3Client.copyObject(EXECUTION_BUCKET_NAME, fileToBePublished, PUBLISHED_BUCKET_NAME,
-				zipKey);
-	    
-	    s3Client.getObject(PUBLISHED_BUCKET_NAME, zipKey);
-		S3Object s3Object = new S3Object();
-		s3Object.setObjectContent(getClass().getResourceAsStream("/test.zip"));
-		returns(s3Object);
+	@Before
+	public void setUp() {
+		publishServiceImpl = new PublishServiceImpl(EXECUTION_BUCKET_NAME, PUBLISHED_BUCKET_NAME, s3Client, s3ClientHelper);
+		Deencapsulation.setField(publishServiceImpl, s3PathHelper);
+		//use Deencapsulation in JMockit or use
+		//ReflectionTestUtils.setField(publishServiceImpl, "executionS3PathHelper", s3PathHelper);
+	}
 
-		s3ClientHelper.newPutRequest(anyString, anyString, withInstanceOf(InputStream.class));
-		returns(new S3PutRequestBuilder(null, null, null, s3ClientHelper));
+	@Test
+	public void testPublishExecutionPackage(@Injectable final Execution execution, @Injectable final Package pk) throws IOException {
+		final Build build = createDummyBuild();
+		new Expectations() {
+			{
+				s3PathHelper.getExecutionOutputFilesPath(execution, anyString);
+				returns(new StringBuffer(EXECUTION_OUTPUT));
 
-		s3ClientHelper.useBucketAcl(withInstanceOf(PutObjectRequest.class));
+				s3Client.listObjects(EXECUTION_BUCKET_NAME, anyString);
+				ObjectListing ol = createDummyObjectListing(EXECUTION_OUTPUT + TEST_ZIP);
+				returns(ol);
 
-		s3Client.putObject(withInstanceOf(PutObjectRequest.class));
-		returns(null);
-	}};
-	publishServiceImpl.publishExecutionPackage(execution, pk);
-	
-    }
-    
-    private Build createDummyBuild() {
-	Build build = new Build("test");
-	Product product = new Product("testProduct");
-	Extension extension = new Extension("testExtension");
-	ReleaseCenter releaseCenter = new ReleaseCenter("International Release Center", "International");
-	extension.setReleaseCenter(releaseCenter);
-	product.setExtension(extension);
-	build.setProduct(product);
-	build.getProduct().getExtension().getReleaseCenter().getBusinessKey();
-	return build;
-    }
-    
-    private String getExpectedPublishFileDir(Product product){
-	 //"international/testextension/testproduct/test.zip"
-	    String productName = product.getName();
-	    Extension extension = product.getExtension();
-	    String extensionName =  extension.getName();
-	    String centerName =  extension.getReleaseCenter().getShortName();
-	    StringBuilder publishedFilePath = new StringBuilder();
-	    publishedFilePath.append(centerName);
-	    publishedFilePath.append( PATH_SEPARATOR + extensionName);
-	    publishedFilePath.append(PATH_SEPARATOR + productName);
-	    publishedFilePath.append(PATH_SEPARATOR);
-	    return publishedFilePath.toString();
-	   
-    }
+				s3PathHelper.getExecutionOutputFilePath(execution, anyString, anyString);
+				String fileToBePublished = EXECUTION_OUTPUT + "text.zip";
+				returns(fileToBePublished);
 
-    @Test
-    public void testGetPublishedPackages(){
-	final Product product = createDummyBuild().getProduct();
-	new Expectations(){{
-	    s3Client.listObjects(PUBLISHED_BUCKET_NAME, getExpectedPublishFileDir(product).toLowerCase() );
-	    ObjectListing ol = createDummyObjectListing( getExpectedPublishFileDir(product) + TEST_ZIP);
-	    returns(ol);
-	}};
-	List<String> result = publishServiceImpl.getPublishedPackages(product);
-	Assert.assertEquals("Published file should be there", TEST_ZIP, result.get(0));
-    }
+				execution.getBuild();
+				returns(build);
 
-    private ObjectListing createDummyObjectListing( String keyName) {
-	ObjectListing ol = new ObjectListing();
-	S3ObjectSummary summary = new S3ObjectSummary();
-	summary.setKey(keyName);
-	ol.getObjectSummaries().add(summary);
-	return ol;
-    }
+				String zipKey = getExpectedPublishFileDir(build.getProduct()).toLowerCase() + TEST_ZIP;
+				s3Client.copyObject(EXECUTION_BUCKET_NAME, fileToBePublished, PUBLISHED_BUCKET_NAME,
+						zipKey);
+
+				s3Client.getObject(PUBLISHED_BUCKET_NAME, zipKey);
+				S3Object s3Object = new S3Object();
+				s3Object.setObjectContent(getClass().getResourceAsStream("/test.zip"));
+				returns(s3Object);
+
+				s3ClientHelper.newPutRequest(anyString, anyString, withInstanceOf(InputStream.class));
+				returns(new S3PutRequestBuilder(null, null, null, s3ClientHelper));
+
+				s3ClientHelper.useBucketAcl(withInstanceOf(PutObjectRequest.class));
+
+				s3Client.putObject(withInstanceOf(PutObjectRequest.class));
+				returns(null);
+			}
+		};
+		publishServiceImpl.publishExecutionPackage(execution, pk);
+
+	}
+
+	private Build createDummyBuild() {
+		Build build = new Build("test");
+		Product product = new Product("testProduct");
+		Extension extension = new Extension("testExtension");
+		ReleaseCenter releaseCenter = new ReleaseCenter("International Release Center", "International");
+		extension.setReleaseCenter(releaseCenter);
+		product.setExtension(extension);
+		build.setProduct(product);
+		build.getProduct().getExtension().getReleaseCenter().getBusinessKey();
+		return build;
+	}
+
+	private String getExpectedPublishFileDir(Product product) {
+		//"international/testextension/testproduct/test.zip"
+		String productName = product.getName();
+		Extension extension = product.getExtension();
+		String extensionName = extension.getName();
+		String centerName = extension.getReleaseCenter().getShortName();
+		StringBuilder publishedFilePath = new StringBuilder();
+		publishedFilePath.append(centerName);
+		publishedFilePath.append(PATH_SEPARATOR + extensionName);
+		publishedFilePath.append(PATH_SEPARATOR + productName);
+		publishedFilePath.append(PATH_SEPARATOR);
+		return publishedFilePath.toString();
+
+	}
+
+	@Test
+	public void testGetPublishedPackages() {
+		final Product product = createDummyBuild().getProduct();
+		new Expectations() {{
+			s3Client.listObjects(PUBLISHED_BUCKET_NAME, getExpectedPublishFileDir(product).toLowerCase());
+			ObjectListing ol = createDummyObjectListing(getExpectedPublishFileDir(product) + TEST_ZIP);
+			returns(ol);
+		}};
+		List<String> result = publishServiceImpl.getPublishedPackages(product);
+		Assert.assertEquals("Published file should be there", TEST_ZIP, result.get(0));
+	}
+
+	private ObjectListing createDummyObjectListing(String keyName) {
+		ObjectListing ol = new ObjectListing();
+		S3ObjectSummary summary = new S3ObjectSummary();
+		summary.setKey(keyName);
+		ol.getObjectSummaries().add(summary);
+		return ol;
+	}
 
 }
