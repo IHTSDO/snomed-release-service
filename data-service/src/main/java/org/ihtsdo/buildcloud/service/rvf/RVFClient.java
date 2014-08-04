12fc7f4a2f6ff4ee2e47bae4781aba3700aed6db
Kai Kewley <kaikewley@gmail.com>
2014-08-04 12:34:29 +0100

    BUG: Correctly reporting RVF pre condition errors.

--- data-service/src/main/java/org/ihtsdo/buildcloud/service/rvf/RVFClient.java
+++ data-service/src/main/java/org/ihtsdo/buildcloud/service/rvf/RVFClient.java
@@ -7,22 +7,21 @@ import org.apache.http.entity.mime.content.InputStreamBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.ihtsdo.buildcloud.dao.io.AsyncPipedStreamBean;
+import org.ihtsdo.buildcloud.service.execution.RF2Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StreamUtils;
 
-import java.io.Closeable;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.concurrent.ExecutionException;
 
 public class RVFClient implements Closeable {
 
 	private final String releaseValidationFrameworkUrl;
 	private final CloseableHttpClient httpClient;
+	private static final String ERROR_NO_LINES_RECEIVED_FROM_RVF = "Error - No lines received from RVF!";
+	private static final String SUCCESS = "Success";
 
-	private static final int SENSIBLE_AMOUNT_OF_DATA = 50;
 	private static final Logger LOGGER = LoggerFactory.getLogger(RVFClient.class);
 
 	public RVFClient(String releaseValidationFrameworkUrl) {
@@ -38,22 +37,38 @@ public class RVFClient implements Closeable {
 
 		try (CloseableHttpResponse response = httpClient.execute(post)) {
 			int statusCode = response.getStatusLine().getStatusCode();
+			long failureCount = 0;
 
 			try (InputStream content = response.getEntity().getContent();
-				 OutputStream logOutputStream = logFileOutputStream.getOutputStream()) {
-				// Did we get a sensible amount of data back from the RVF?
-				if (content.available() < SENSIBLE_AMOUNT_OF_DATA) {
-					StreamUtils.copy("Suspiciously small content warning.\n".getBytes(), logOutputStream);
+				 BufferedReader responseReader = new BufferedReader(new InputStreamReader(content));
+				 BufferedWriter logWriter = new BufferedWriter(new OutputStreamWriter(logFileOutputStream.getOutputStream()))) {
+
+				String line = responseReader.readLine(); // read header
+				if (line != null) {
+					while ((line = responseReader.readLine()) != null) {
+						if (!line.startsWith(SUCCESS)) {
+							failureCount++;
+						}
+						logWriter.write(line);
+						logWriter.write(RF2Constants.LINE_ENDING);
+					}
+				} else {
+					logWriter.write(ERROR_NO_LINES_RECEIVED_FROM_RVF);
+					logWriter.write(RF2Constants.LINE_ENDING);
 				}
-				StreamUtils.copy(content, logOutputStream);
 			}
 			logFileOutputStream.waitForFinish();
 
 			if (200 == statusCode) {
-				return null;
+				if (failureCount == 0) {
+					return null;
+				} else {
+					return "There were " + failureCount + " RVF test failures.";
+				}
 			} else {
-				LOGGER.info("RVF Service failure: HTTP {}", statusCode);
-				return "" + statusCode;
+				String errorMessage = "Response HTTP status code " + statusCode;
+				LOGGER.info("RVF Service failure: {}", errorMessage);
+				return errorMessage;
 			}
 		} catch (InterruptedException | ExecutionException | IOException e) {
 			String errorMessage = "Failed to check input file against RVF: " + inputFileName + " due to " + e.getMessage();
