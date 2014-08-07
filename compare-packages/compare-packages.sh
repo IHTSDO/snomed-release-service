acbfd76509dfe8d12ee3d00c21937cbf22eefc0d
Peter G. Williams <pwi@ihtsdo.org>
2014-08-07 13:39:48 +0100

    Now comparing files using flat directory structure.  Also outputting line count of differences - see "c" directory for details.

--- compare-packages/compare-packages.sh
+++ compare-packages/compare-packages.sh
@@ -20,64 +20,55 @@ function extractZip {
 function createLists {
 	listName=$1
 	dir=$2
-	find $dir -type d > target/${listName}_dir_list.txt
-	find $dir -type f > target/${listName}_file_list.txt
+	home=`pwd`
+	cd $dir
+	find . -type d > ${home}/target/${listName}_dir_list.txt
+	find . -type f > ${home}/target/${listName}_file_list.txt
+	cd - > /dev/null
 }
 
-function normaliseLegacyStructure {
-	targetDir=$1
-	mkdir -p $targetDir
-	
-	# Copy files from their download location to the appropriate location
-	moveLegacyFiles "Delta" $targetDir
-	moveLegacyFiles "Snapshot"	$targetDir
-	moveLegacyFiles "Full"	$targetDir
-}
 
-function moveLegacyFiles {
-	fileType=$1
-	targetDir=$2	
-	
-	echo "Moving ${fileType} legacy files from ${legacyDir} to ${targetDir}"
-	# First move the content files
-	thisTarget="${targetDir}/RF2Release/${fileType}/Terminology/"
-	mkdir -p $thisTarget
-	mv -v ${legacyDir}/${fileType}/sct2*  ${thisTarget}
+extractZip $srsArchive "target"
 
-	#Next the Map, Language and the content Refsets
-	thisTarget="${targetDir}/RF2Release/${fileType}/Refset/Language/"
-	mkdir -p $thisTarget
-	mv -v ${legacyDir}/${fileType}/der2*Language* $thisTarget
-	
-	thisTarget="${targetDir}/RF2Release/${fileType}/Refset/Map/"
-	mkdir -p $thisTarget
-	mv -v ${legacyDir}/${fileType}/der2*SimpleMap* $thisTarget
-	
-	thisTarget="${targetDir}/RF2Release/${fileType}/Refset/Content/"
-	mkdir -p $thisTarget
-	mv -v ${legacyDir}/${fileType}/der2* $thisTarget
-}
+srsLocation="target/SRS_Daily_Build_20150131"
+legacyLocation="${legacyDir}/destination"
 
-extractZip $srsArchive "target/a"
-normaliseLegacyStructure "target/b"
+#Flatten the SRS structure into directory "a"
+mkdir -p target/a
+echo "Moving SRS into flat structure 'a'"
+find ${srsLocation} -type f | xargs -I {} mv {} target/a
+
+#Move the legacy structure into "b"
+mkdir -p target/b
+echo "Moving Legacy Build into flat structure 'b'"
+find ${legacyLocation} -type f | xargs -I {} mv {} target/b
+
+createLists "srs" "target/a"
+createLists "legacy" "target/b"
+
+echo -e "_Directory list differences SRS vs Legacy_\n"
+diff target/srs_dir_list.txt target/legacy_dir_list.txt && echo "None"
 echo
 
-createLists "srs" "target/a/SRS_Daily_Build_20150131/RF2Release/"
-createLists "legacy" "target/b/RF2Release/"
-
-echo -e "_Directory list differences_\n"
-diff target/srs_dir_list.txt target/legacy_dir_list.txt || echo
+echo -e "_File list differences SRS vs Legacy_\n"
+diff target/srs_file_list.txt target/legacy_file_list.txt && echo "None"
 echo
 
-echo -e "_File list differences_\n"
-diff target/srs_file_list.txt target/legacy_file_list.txt
-echo
-
-#echo -e "_File content differences_\n"
-#for file in `find a/extract -type f | sed 's/a\///'`; do
-#	echo "File" $file
-#	sort a/$file > tmp.txt && mv tmp.txt a/$file
-#	sort b/$file > tmp.txt && mv tmp.txt b/$file
-#	diff a/$file b/$file && echo "None"
-#	echo
-#done
+mkdir -p target/c
+echo -e "_File content differences_\n"
+for file in `find target/a -type f | sed "s/target\/a\///"`; do
+	leftFile="target/a/${file}"
+	rightFile="target/b/${file}"
+	if [ -f "${rightFile}" ]
+	then 
+		echo "Comparing File" $file
+		sort ${leftFile} > tmp.txt 
+		mv tmp.txt ${leftFile}
+		sort ${rightFile} > tmp.txt 
+		mv tmp.txt ${rightFile}
+		echo -n "${file} differences count: "
+		diff  ${leftFile} ${rightFile} | tee target/c/diff_${file} | wc -l 
+	else
+		echo "Skipping file ${file} - no counterpart in the legacy build"
+	fi
+done
