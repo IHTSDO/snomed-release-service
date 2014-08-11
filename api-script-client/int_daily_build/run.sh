59381b5208338a7a20569dff312decc38b911578
Kai Kewley <kaikewley@gmail.com>
2014-08-11 11:23:55 +0100

    Daily build: Set target environment through args.

--- api-script-client/int_daily_build/run.sh
+++ api-script-client/int_daily_build/run.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 set -e; # Stop on error
 
+# Command line args
+dataLocation=$1
+host=$2
+if [ -n "$host" ]; then
+	clientOpts="-h $host"
+else
+	clientOpts=""
+fi
+
 # Declare run specific parameters
 effectiveDate="2015-01-31"
 readmeEndDate="2014"
 isFirstTime=false
 headless=true
 extensionName="SNOMED CT International Edition"
-dataLocation=$1
 previousPublishedPackageName="SnomedCT_Release_INT_20140731.zip"
 productName="SNOMED CT Release"
 buildName="Int Daily Build"
@@ -16,4 +24,4 @@ packageName="Snomed Release Package"
 echo "Configuration set to pull export files from $1"
 
 # Call api_client
-source ../api_client.sh
+source ../api_client.sh $clientOpts
