f04957cf500a3bde3ff49c58042362e5df847c46
Peter G. Williams <pwi@ihtsdo.org>
2014-08-20 13:41:10 +0200

    Add script for investigating daily build issues

---
+++ api-script-client/investigate_daily_build/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e; # Stop on error
+
+# Command line args
+dataLocation=$1
+host=$2
+if [ -n "$host" ]; then
+	clientOpts="-h $host"
+else
+	clientOpts=""
+fi
+
+# Declare run specific parameters
+effectiveDate="2015-01-31"
+readmeEndDate="2014"
+isFirstTime=false
+isWorkbenchDataFixesRequired=true
+headless=true
+extensionName="SNOMED CT International Edition"
+previousPublishedPackageName="SnomedCT_Release_INT_20140731.zip"
+productName="SNOMED CT Release"
+buildName="Investigate Daily Build"
+packageName="Snomed Release Package"
+
+echo "Configuration set to pull export files from $1"
+
+# Call api_client
+source ../api_client.sh $clientOpts
