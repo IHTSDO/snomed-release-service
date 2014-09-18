6701a88278692ee83c7d5156a46870a3252300f3
Kai Kewley <kaikewley@gmail.com>
2014-09-18 10:05:01 +0100

    Equals and hashCode methods in SCTIDKey - sonar issue.

--- data-service/src/main/java/org/ihtsdo/buildcloud/service/execution/database/map/SCTIDKey.java
+++ data-service/src/main/java/org/ihtsdo/buildcloud/service/execution/database/map/SCTIDKey.java
@@ -21,6 +21,26 @@ public class SCTIDKey implements Key {
 	}
 
 	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		SCTIDKey sctidKey = (SCTIDKey) o;
+
+		if (effectiveTime != null ? !effectiveTime.equals(sctidKey.effectiveTime) : sctidKey.effectiveTime != null) return false;
+		if (id != null ? !id.equals(sctidKey.id) : sctidKey.id != null) return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = id != null ? id.hashCode() : 0;
+		result = 31 * result + (effectiveTime != null ? effectiveTime.hashCode() : 0);
+		return result;
+	}
+
+	@Override
 	public String getIdString() {
 		return id.toString();
 	}
