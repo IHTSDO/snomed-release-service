89ba680207e212e4e8b9b38ec88f4746687704d3
CoderMChu <mch@ihtsdo.org>
2014-12-01 17:37:52 +0000

    Add core subsequent release script client and test data

---
+++ api-script-client/core_subsequent_release/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e; # Stop on error
+
+# Declare run specific parameters
+productName="Core"
+effectiveDate="2014-07-31"
+readmeEndDate="2014"
+isFirstTime=false
+createInferredRelationships=true
+createLegacyIds=true
+newRF2InputFiles="rel2_Refset_SimpleDelta_INT_20140731.txt"
+previousPublishedPackageName="ExampleCore_Release_INT_20140131.zip"
+# Call api_client
+source ../api_client.sh
