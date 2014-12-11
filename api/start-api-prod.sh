3fef863e585f4b9c2bcfbbdf7524feffd02a39d8
Peter G. Williams <pwi@ihtsdo.org>
2014-12-11 18:04:17 +0000

    Enhance local API starters to build root pom and pass correct properties file.

--- api/start-api-prod.sh
+++ api/start-api-prod.sh
@@ -15,7 +15,7 @@ read tunnelUserName
 echo "What port is nginx expecting to find the api on? "
 read apiPort
 
-propertiesFile="../data-service/target/classes/${filename}"
+propertiesFile="`pwd`/../data-service/target/classes/${filename}"
 if [ -f "$propertiesFile" ]; then
 
 	# Check if tunnel is already established before running
@@ -37,7 +37,7 @@ if [ -f "$propertiesFile" ]; then
 	echo "Starting API webapp using $environmentName environment."
 	echo
 	sleep 1
-	java -Xdebug -Xnoagent -Xmx4g -DENV_NAME=PGW -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -jar target/exec-api.jar -DdataServicePropertiesPath="file://${propertiesFile}"  -httpPort=${apiPort}
+	java -Xdebug -Xnoagent -Xmx4g -DENV_NAME=LOCAL -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -jar target/exec-api.jar -DdataServicePropertiesPath="file://${propertiesFile}"  -httpPort=${apiPort}
 else
 	echo "You don't have access to the $environmentName environment."
 	echo
