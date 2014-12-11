85b421fae39741469525eec29f9500184f0693f4
Peter G. Williams <pwi@ihtsdo.org>
2014-12-11 18:18:25 +0000

    Revert "Enhance local API starters to build root pom and pass correct properties file."
    This reverts commit 48851ccf55c82233c163036dd4d7d2e70bea6843.

--- api/start-api-uat.sh
+++ api/start-api-uat.sh
@@ -4,19 +4,16 @@ set -e
 environmentName="uat"
 filename="${environmentName}-data-service.properties"
 
-echo "What port is nginx expecting to find the api on? "
-read apiPort
-
 echo 'Building API webapp (skipping tests)..'
 sleep 1
-mvn -f ../pom.xml clean install -Dapple.awt.UIElement='true' -DskipTests=true
+mvn clean install -Dapple.awt.UIElement='true' -DskipTests=true
 echo
-propertiesFile="`pwd`/../data-service/target/classes/${filename}"
+propertiesFile="`pwd`/$filename"
 if [ -f "$propertiesFile" ]; then
 	echo "Starting API webapp using $environmentName environment."
 	echo
 	sleep 1
-	java -Xdebug -Xnoagent -Xmx4g -DENV_NAME=LOCAL -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -jar target/exec-api.jar -DdataServicePropertiesPath="file://${propertiesFile}"  -httpPort=${apiPort}
+	java -jar target/exec-api.jar -DdataServicePropertiesPath="file://$propertiesFile"
 else
 	echo "You don't have access to the $environmentName environment."
 	echo
