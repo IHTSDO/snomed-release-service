3f88ea3b704f200af6eb9c95532d702a67b09730
CoderMChu <mch@ihtsdo.org>
2014-12-12 09:21:23 +0000

    Create srs db change log using liquibase

--- api-script-client/core_first_release/run.sh
+++ api-script-client/core_first_release/run.sh
@@ -3,10 +3,10 @@ set -e; # Stop on error
 
 # Declare run specific parameters
 productName="Core"
-effectiveDate="2014-01-31"
+effectiveDate="2015-01-31"
 readmeEndDate="2014"
 isFirstTime=true
 createInferredRelationships=true
-createLegacyIds=false
+createLegacyIds=true
 # Call api_client
 source ../api_client.sh
