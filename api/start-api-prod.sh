a8f9521a1aca1c351cf67fcac7cca72b01013187
Peter G. Williams <pwi@ihtsdo.org>
2015-01-05 11:11:35 +0000

    Parameterised debug flag and port number.

--- api/start-api-prod.sh
+++ api/start-api-prod.sh
@@ -2,6 +2,7 @@
 set -e
 
 environmentName="prod"
+apiPort=8080
 filename="${environmentName}-data-service.properties"
 
 function getProperty() {
@@ -9,11 +10,29 @@ function getProperty() {
 	cat ${propertiesFile} | grep ${property} | awk '{print $2}'
 }
 
+while getopts ":dp:" opt
+do
+	case $opt in
+		d) 
+			debugMode=true
+			echo "Option set to start API in debug mode."
+			debugFlags="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8001 -Djava.compiler=NONE" 
+		;;
+		p)
+			apiPort=$OPTARG
+			echo "Option set run API on port ${apiPort}"
+		;;
+		help|\?)
+			echo -e "Usage: [-d]  [-p <port>]"
+			echo -e "\t d - debug. Starts the API in debug mode, which an IDE can attach to on port 8001"
+			echo -e "\t p <port> - Starts the API on a specific port (default 8080)"
+			exit 0
+		;;
+	esac
+done
+
 echo "Please enter a username under which to set up the id-gen tunnel: "
 read tunnelUserName
-
-echo "What port is nginx expecting to find the api on? "
-read apiPort
 
 propertiesFile="`pwd`/../data-service/target/classes/${filename}"
 if [ -f "$propertiesFile" ]; then
@@ -34,10 +53,10 @@ if [ -f "$propertiesFile" ]; then
 	mvn -f ../pom.xml clean install -Dapple.awt.UIElement='true' -DskipTests=true
 	echo
 
-	echo "Starting API webapp using $environmentName environment."
+	echo "Starting API webapp using $environmentName environment on port ${apiPort}."
 	echo
 	sleep 1
-	java -Xdebug -Xnoagent -Xmx4g -DENV_NAME=LOCAL -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -jar target/exec-api.jar -DdataServicePropertiesPath="file://${propertiesFile}"  -httpPort=${apiPort}
+	java ${debugFlags} -Xmx4g -DENV_NAME=$(whoami) -jar target/exec-api.jar -DdataServicePropertiesPath="file://${propertiesFile}"  -httpPort=${apiPort}
 else
 	echo "You don't have access to the $environmentName environment."
 	echo
