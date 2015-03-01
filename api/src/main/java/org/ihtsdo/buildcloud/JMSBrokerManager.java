4df88520683e10a9b99f388ab8a2f73252f68c79
Peter G. Williams <pwi@ihtsdo.org>
2015-03-01 08:52:00 +0000

    Liquidbase more likely to have caused problem than JMS, so removing unecessary fix

--- api/src/main/java/org/ihtsdo/buildcloud/JMSBrokerManager.java
+++ api/src/main/java/org/ihtsdo/buildcloud/JMSBrokerManager.java
@@ -1,7 +1,6 @@
 package org.ihtsdo.buildcloud;
 
 import org.apache.activemq.broker.BrokerService;
-import org.apache.activemq.usage.SystemUsage;
 
 public class JMSBrokerManager {
 
@@ -12,10 +11,13 @@ public class JMSBrokerManager {
 		// 20150227_17:19:21 ERROR BrokerService - Temporary Store limit is 51200 mb, whilst the temporary data directory:
 		// /mnt/pd1/var/opt/snomed-release-service-api/run/activemq-data/localhost/tmp_storage only has 38992 mb of usable space - resetting
 		// to maximum available 38992 mb.
-		broker.setPersistent(false);
-		SystemUsage systemUsage = broker.getSystemUsage();
-		systemUsage.getStoreUsage().setLimit(1024 * 1024 * 8);
-		systemUsage.getTempUsage().setLimit(1024 * 1024 * 8);
+
+		// This may not have caused the problem - more likely to be with Liquidbase lock, so taking back out for now...
+		/*
+		 * broker.setPersistent(false); SystemUsage systemUsage = broker.getSystemUsage(); systemUsage.getStoreUsage().setLimit(1024 * 1024
+		 * * 8); systemUsage.getTempUsage().setLimit(1024 * 1024 * 8);
+		 */
+
 		broker.start();
 	}
 }
