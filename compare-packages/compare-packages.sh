ab54b479f750fdbd579f15220e2817be719dc440
Peter G. Williams <pwi@ihtsdo.org>
2015-03-11 11:30:55 +0000

    Made more generic so any two equivalent releases can be compared

--- compare-packages/compare-packages.sh
+++ compare-packages/compare-packages.sh
@@ -20,47 +20,65 @@ function createLists {
 rm -rf target || true
 mkdir -p target
 
-if [ $# -ne 2 ]; then
-	echo "Usage: compare-packages.sh <SRS Zip Archive location> <Legacy Build or Download Directory>"
+if [ $# -lt 4 ]; then
+	echo "Usage: compare-packages.sh <Left Name eg SRS> <Left Archive location> <Right Name eg Legacy> <Right Archive or Download Directory>"
 	exit 1
 fi
 
-srsArchive=$1
-legacyData=$2
+leftName=$1
+leftArchive=$2
+rightName=$3
+rightArchive=$4
+flags=$5
 
-srsLocation="target/left_archive"
-extractZip ${srsArchive} ${srsLocation}
-
-#Did we in fact get passed two zip files for comparison?  Extract if so
-if [[ $legacyData =~ \.zip$ ]]
-then 
-	legacyDir="target/right_archive" 
-	echo "2nd archive detected instead of directory, resolving to ${legacyDir}"
-	unzip $legacyData -d ${legacyDir}
-else
-	legacyDir=${legacyData}
+if [ "${flags}" == "-normaliseDates" ]; then
+	normaliseDates=true
+	echo "Option set to make effective dates in filenames the same"
 fi
 
-#Flatten the SRS structure into directory "a"
+leftLocation="target/left_archive"
+extractZip ${leftArchive} ${leftLocation}
+
+#Did we in fact get passed two zip files for comparison?  Extract if so
+if [[ $rightArchive =~ \.zip$ ]]
+then 
+	rightDir="target/right_archive" 
+	echo "2nd archive detected instead of directory, resolving to ${rightDir}"
+	unzip $rightArchive -d ${rightDir}
+else
+	rightDir=${rightArchive}
+fi
+
+#Flatten the left structure into directory "a"
 mkdir -p target/a
-echo "Moving SRS into flat structure 'a'"
-find ${srsLocation} -type f | xargs -I {} mv {} target/a
+echo "Moving ${leftName} into flat structure 'a'"
+find ${leftLocation} -type f | xargs -I {} mv {} target/a
 
-#Move the legacy structure into "b"
+#Move the right structure into "b"
 mkdir -p target/b
-echo "Moving Legacy Build into flat structure 'b'"
-find ${legacyDir} -type f | xargs -I {} mv {} target/b
+echo "Moving ${rightName} into flat structure 'b'"
+find ${rightDir} -type f | xargs -I {} mv {} target/b
 
-createLists "srs" "target/a"
-createLists "legacy" "target/b"
+#If we're normalising the dates, find the date from a file in a and set all the 
+#files in b to have the same date
+if [ "${normaliseDates}" == true ]; then
+	effectiveDate=`ls -1 target/a | head -1 | sed  's/.*\([0-9]\{8\}\).*/\1/'`
+	for thisFile in target/b/* ; do
+		newFileName=`echo ${thisFile} | sed "s/\([0-9]\{8\}\)/${effectiveDate}/"`
+		mv -v ${thisFile} ${newFileName}
+	done
+fi
 
-echo -e "_File list differences SRS vs Legacy_\n"
-diff target/srs_file_list.txt target/legacy_file_list.txt && echo "None"
+createLists ${leftName} "target/a"
+createLists ${rightName} "target/b"
+
+echo -e "_File list differences ${leftName} vs ${rightName}_\n"
+diff target/${leftName}_file_list.txt target/${rightName}_file_list.txt && echo "None"
 echo
 
 mkdir -p target/c
 echo "_File content differences_"
-echo "Line count diff is SRS minus WBRP"
+echo "Line count diff is ${leftName} minus ${rightName}"
 echo
 processOrderFile="_process_order.txt"
 find target/a -type f | sed "s/target\/a\///" | grep "sct2_" | sort > ${processOrderFile}
@@ -77,10 +95,10 @@ for file in `cat ${processOrderFile}`; do
 		echo "Comparing ${file}"
 
 		leftFileCount=`wc -l ${leftFile} | awk '{print $1}'`
-		echo "SRS line count: $leftFileCount"
+		echo "${leftName} line count: $leftFileCount"
 
 		rightFileCount=`wc -l ${rightFile} | awk '{print $1}'`
-		echo "WBRP line count: $rightFileCount"
+		echo "${rightName} line count: $rightFileCount"
 
 		echo "Line count diff: $[$leftFileCount-$rightFileCount]"
 
@@ -102,7 +120,7 @@ for file in `cat ${processOrderFile}`; do
 		fi
 		echo
 	else
-		echo "Skipping ${file} - no counterpart in the legacy build"
+		echo "Skipping ${file} - no counterpart in ${rightName}"
 		echo
 	fi
 done
