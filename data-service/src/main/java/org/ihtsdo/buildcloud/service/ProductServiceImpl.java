0584eb9f12262702ae0128d608614bf58827a104
CoderMChu <mch@ihtsdo.org>
2015-07-27 17:56:04 +0100

    Fix qa test config not being udpated correctly

--- data-service/src/main/java/org/ihtsdo/buildcloud/service/ProductServiceImpl.java
+++ data-service/src/main/java/org/ihtsdo/buildcloud/service/ProductServiceImpl.java
@@ -105,18 +105,9 @@ public class ProductServiceImpl extends EntityServiceImpl<Product> implements Pr
 			qaTestConfig.setProduct(product);
 			product.setQaTestConfig(qaTestConfig);
 		}
-		if (newPropertyValues.containsKey(ProductService.PREVIOUS_INTERNATIONAL_RELEASE)) {
-			qaTestConfig.setPreviousInternationalRelease(newPropertyValues.get(ProductService.PREVIOUS_INTERNATIONAL_RELEASE));
-		}
-		if (newPropertyValues.containsKey(ProductService.PREVIOUS_EXTENSION_RELEASE)) {
-			qaTestConfig.setPreviousExtensionRelease(newPropertyValues.get(ProductService.PREVIOUS_EXTENSION_RELEASE));
-		}
-		if (newPropertyValues.containsKey(ProductService.ASSERTION_GROUP_NAMES)) {
-			qaTestConfig.setAssertionGroupNames(newPropertyValues.get(ProductService.ASSERTION_GROUP_NAMES));
-		}
-		if (newPropertyValues.containsKey(ProductService.EXTENSION_DEPENDENCY_RELEASE)) {
-			qaTestConfig.setExtensionDependencyRelease(newPropertyValues.get(ProductService.EXTENSION_DEPENDENCY_RELEASE));
-		}
+		qaTestConfig.setPreviousInternationalRelease(newPropertyValues.get(ProductService.PREVIOUS_INTERNATIONAL_RELEASE));
+		qaTestConfig.setAssertionGroupNames(newPropertyValues.get(ProductService.ASSERTION_GROUP_NAMES));
+		qaTestConfig.setExtensionDependencyRelease(newPropertyValues.get(ProductService.EXTENSION_DEPENDENCY_RELEASE));
 	}
 
 	private void updateProductBuildConfiguration(final Map<String, String> newPropertyValues, final Product product)
