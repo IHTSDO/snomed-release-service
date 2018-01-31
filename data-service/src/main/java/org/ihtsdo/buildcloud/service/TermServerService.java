97ea4292df23dabab4d1039c82561956f4bf6b8e
Huy Le <huyhu@netcompany.com>
2018-01-31 17:45:45 +0700

    Fixed unit tests

--- data-service/src/main/java/org/ihtsdo/buildcloud/service/TermServerService.java
+++ data-service/src/main/java/org/ihtsdo/buildcloud/service/TermServerService.java
@@ -9,8 +9,8 @@ import java.util.Set;
 
 public interface TermServerService {
 
-    File export(String snowowlUrl, String branchPath, String startEffectiveDate, String endEffectiveDate, String effectiveDate, Set<String> moduleIds, SnowOwlRestClient.ExportCategory exportCategory,
-                SnowOwlRestClient.ExportType exportType, String namespaceId, Boolean includeUnpublished, String codeSystemShortName) throws BusinessServiceException, FileNotFoundException;
+    /*File export(String snowowlUrl, String branchPath, String startEffectiveDate, String endEffectiveDate, String effectiveDate, Set<String> moduleIds, SnowOwlRestClient.ExportCategory exportCategory,
+                SnowOwlRestClient.ExportType exportType, String namespaceId, Boolean includeUnpublished, String codeSystemShortName) throws BusinessServiceException, FileNotFoundException;*/
 
     File export(String termServer, String branchPath, String effectiveDate, Set<String> excludedModuleId, SnowOwlRestClient.ExportCategory exportCategory) throws BusinessServiceException;
 
