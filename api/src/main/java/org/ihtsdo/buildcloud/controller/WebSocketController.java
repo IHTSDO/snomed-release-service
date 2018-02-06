187a202f1385e1deef3405f60e116ac4d8d33cb7
Huy Le <huy.le@netcompany.com>
2018-02-06 15:02:27 +0700

    Added websocket to trace logs

---
+++ api/src/main/java/org/ihtsdo/buildcloud/controller/WebSocketController.java
@@ -0,0 +1,10 @@
+package org.ihtsdo.buildcloud.controller;
+
+/**
+ * User: huyle
+ * Date: 2/5/18
+ * Time: 1:14 PM
+ */
+public class WebSocketController {
+
+}
---
+++ api/src/main/java/org/ihtsdo/buildcloud/controller/helper/LogViewerHandler.java
@@ -0,0 +1,10 @@
+package org.ihtsdo.buildcloud.controller.helper;
+
+/**
+ * User: huyle
+ * Date: 2/2/18
+ * Time: 2:54 PM
+ */
+public class LogViewerHandler {
+
+}
