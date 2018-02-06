3f1d56bf8b626c4722d96b2d60a954a282ac2fb2
Huy Le <huy.le@netcompany.com>
2018-02-06 15:02:27 +0700

    Added websocket to trace logs

---
+++ api/src/main/java/org/ihtsdo/buildcloud/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package org.ihtsdo.buildcloud.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketconfig extends AbstractWebSocketMessageBrokerConfigurer{
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+        stompEndpointRegistry.addEndpoint("/chat");
+        stompEndpointRegistry.addEndpoint("/chat").withSockJS();
+    }
+}
