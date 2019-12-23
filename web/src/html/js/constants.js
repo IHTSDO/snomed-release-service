63f4893d08838e202348a5cbc40d233a59fa989b
HM-huyle <huyhu@netcompany.com>
2019-12-23 17:23:19 +0700

    Added web form UI for triggerring builds

---
+++ web/src/html/js/constants.js
@@ -0,0 +1,4 @@
+var SRS_HM_URL="https://uat-release-hm.ihtsdotools.org";
+var TERM_SERVER_INT_URL="https://prod-snowstorm.ihtsdotools.org";
+var TERM_SERVER_MS_URL="https://prod-ms-authoring.ihtsdotools.org";
+var RELEASE_CENTER_INT="international";
