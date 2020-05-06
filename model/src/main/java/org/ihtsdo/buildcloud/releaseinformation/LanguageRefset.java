9f062871a6f87487d1561fdc6a50d21f6778163f
Quyen Ly <quocquyenit@gmail.com>
2020-05-06 17:02:54 +0700

    SRS-589 Used string instead number to display ID

--- model/src/main/java/org/ihtsdo/buildcloud/releaseinformation/LanguageRefset.java
+++ model/src/main/java/org/ihtsdo/buildcloud/releaseinformation/LanguageRefset.java
@@ -3,15 +3,15 @@ package org.ihtsdo.buildcloud.releaseinformation;
 import java.math.BigInteger;
 
 public class LanguageRefset {
-    private BigInteger id;
+    private String id;
 
     private String term;
 
-    public BigInteger getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(BigInteger id) {
+    public void setId(String id) {
         this.id = id;
     }
 
