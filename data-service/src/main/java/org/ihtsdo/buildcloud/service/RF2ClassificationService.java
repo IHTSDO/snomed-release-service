580f2527e1858bcbddb945c0247a58261ed5066c
CoderMChu <mch@ihtsdo.org>
2020-10-29 16:10:52 +0000

    CDI-50 Remove duplicate code and refactor classification results checking process

--- data-service/src/main/java/org/ihtsdo/buildcloud/service/RF2ClassificationService.java
+++ data-service/src/main/java/org/ihtsdo/buildcloud/service/RF2ClassificationService.java
@@ -1,10 +1,11 @@
 package org.ihtsdo.buildcloud.service;
 
 import com.google.common.io.Files;
+import org.apache.commons.io.FileUtils;
 import org.ihtsdo.buildcloud.dao.BuildDAO;
 import org.ihtsdo.buildcloud.entity.Build;
 import org.ihtsdo.buildcloud.entity.ExtensionConfig;
-import org.ihtsdo.buildcloud.service.classifier.ExternalRF2ClassifierRestClient;
+import org.ihtsdo.buildcloud.service.classifier.ClassificationServiceRestClient;
 import org.ihtsdo.otf.rest.exception.BusinessServiceException;
 import org.ihtsdo.otf.rest.exception.ProcessingException;
 import org.ihtsdo.otf.utils.ZipFileUtils;
@@ -18,7 +19,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.ihtsdo.buildcloud.service.build.RF2Constants.*;
@@ -27,7 +27,7 @@ import static org.ihtsdo.buildcloud.service.build.RF2Constants.*;
 public class RF2ClassificationService {
 
 	@Autowired
-	private ExternalRF2ClassifierRestClient classifierRestClient;
+	private ClassificationServiceRestClient classificationRestClient;
 
 	@Autowired
 	private BuildDAO buildDAO;
@@ -37,14 +37,14 @@ public class RF2ClassificationService {
 
 	private static final String OWL_REFSET_FILE_PATTERN = ".*_sRefset_OWL.*";
 
-	private static final String REL2_MRCM_ATTRIBUTE_DOMAIN_DELTA = "rel2_cissccRefset_MRCMAttributeDomainDelta";
+	private static final String MRCM_ATTRIBUTE_DOMAIN_DELTA = ".*_cissccRefset_MRCMAttributeDomainDelta";
 
-	private static final String REL2_CONCEPT_PATTERN = ".*rel2_Concept_.*Delta.*";
+	private static final String CONCEPT_PATTERN = ".*_Concept_.*Delta.*";
 
 	// sct2_RelationshipConcreteValues_Delta or sct2_Relationship_Delta
-	private static final String REL2_RELATIONSHIP_PATTERN = ".*rel2_Relationship.*Delta.*";
+	private static final String RELATIONSHIP_PATTERN = ".*_Relationship.*Delta.*";
 
-	private static final String REL2_STATED_RELATIONSHIP_PATTERN = ".*rel2_StatedRelationship_.*Delta.*";
+	private static final String STATED_RELATIONSHIP_PATTERN = ".*_StatedRelationship_.*Delta.*";
 
 	private static final String REFSET = "Refset_";
 
@@ -52,8 +52,15 @@ public class RF2ClassificationService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(RF2ClassificationService.class);
 
-	public File classify(Build build) throws BusinessServiceException {
-		LOGGER.info("Run classification for build {}", build.getProduct() + build.getId());
+	private static final String ERROR_MSG_FORMAT = "Inconsistencies found in %s file in classification results " +
+			"- expected 0 records but found %d records from classification service." +
+			" Please see detailed failures via the classificationResultsOutputFiles_url link listed.";
+
+	public File classify(Build build, File rf2DeltaFile) throws BusinessServiceException {
+		LOGGER.info("Run classification for product {} and build id {}", build.getProduct(), build.getId());
+		if (!rf2DeltaFile.exists() || !rf2DeltaFile.canRead()) {
+			throw new IllegalArgumentException("File doesn't exist " + rf2DeltaFile.getAbsolutePath());
+		}
 		String previousPublished = build.getConfiguration().getPreviousPublishedPackage();
 		String dependencyRelease = null;
 		ExtensionConfig extensionConfig = build.getConfiguration().getExtensionConfig();
@@ -61,54 +68,111 @@ public class RF2ClassificationService {
 			dependencyRelease = extensionConfig.getDependencyRelease();
 			if (dependencyRelease == null || dependencyRelease.isEmpty()) {
 				if (extensionConfig.isReleaseAsAnEdition()) {
-					LOGGER.warn("The product is configured as an edition without dependency package. Only previous package {} will be used in classification", previousPublished);
+					// This can happen when the previous edition release has got the latest published international release.
+					LOGGER.info("The product is configured as an edition without dependency package." +
+							" Only previous package {} will be used in classification", previousPublished);
 				} else {
-					throw new BusinessServiceException("International dependency release can't be null for extension release build.");
+					throw new BusinessServiceException(String.format("International dependency release can't be null for extension release build %s", build.getProduct()));
 				}
 			}
 		}
-		File deltaTempDir = Files.createTempDir();
 		File zipTempDir = Files.createTempDir();
 		File rf2DeltaZipFile = new File(zipTempDir, "rf2Delta_" + build.getId() + ".zip");
 		// Create a Delta package based on the input files and send to classification service
 		try {
-			createDeltaArchiveForClassification(build, deltaTempDir);
-			ZipFileUtils.zip(deltaTempDir.getAbsolutePath(), rf2DeltaZipFile.getAbsolutePath());
-		} catch (ProcessingException | IOException e) {
-			throw new BusinessServiceException(e);
+			ZipFileUtils.zip(rf2DeltaFile.getAbsolutePath(), rf2DeltaZipFile.getAbsolutePath());
+			// Classify
+			return classificationRestClient.classify(rf2DeltaZipFile, previousPublished, dependencyRelease);
+		} catch (IOException e) {
+			throw new BusinessServiceException("Failed to zip rf2 delta files for classification", e);
+		} finally {
+			FileUtils.deleteQuietly(zipTempDir);
 		}
-		// Classify
-		return classifierRestClient.classify(rf2DeltaZipFile, previousPublished, dependencyRelease);
 	}
 
-		private List<String> createDeltaArchiveForClassification(final Build build, final File deltaTempDir) throws ProcessingException, IOException {
-			LOGGER.info("Creating delta archive for classification");
-			final List<String> localFilePaths = new ArrayList<>();
-			for (String downloadFilename : buildDAO.listInputFileNames(build)) {
-				if (!isRequiredFileForClassification(downloadFilename)) continue;
-				LOGGER.info("Prepare {} for archiving", downloadFilename);
-				String rename = (downloadFilename.contains(REFSET) && !downloadFilename.matches(OWL_REFSET_FILE_PATTERN)) ? downloadFilename.replace(INPUT_FILE_PREFIX, DER2) : downloadFilename.replace(INPUT_FILE_PREFIX, SCT2);
-				LOGGER.info("Rename {} to {} for archiving", downloadFilename, rename);
-				final File localFile = new File(deltaTempDir, rename);
-				try (InputStream inputFileStream = productInputFileService.getFileInputStream(build.getProduct().getReleaseCenter().getBusinessKey()
-						, build.getProduct().getBusinessKey(), downloadFilename);
-				FileOutputStream out = new FileOutputStream(localFile)) {
-					if (inputFileStream != null) {
-						StreamUtils.copy(inputFileStream, out);
-						localFilePaths.add(localFile.getAbsolutePath());
-					} else {
-						throw new ProcessingException("Didn't find input file:" + downloadFilename);
+	public File downloadInputDelta(final Build build) throws ProcessingException, IOException {
+		LOGGER.info("Creating delta archive for classification");
+		final File deltaTempDir = Files.createTempDir();
+		for (String downloadFilename : buildDAO.listInputFileNames(build)) {
+			if (!isRequiredFileForClassification(downloadFilename)) continue;
+			LOGGER.info("Prepare {} for archiving", downloadFilename);
+			String rename = (downloadFilename.contains(REFSET) && !downloadFilename.matches(OWL_REFSET_FILE_PATTERN)) ? downloadFilename.replace(INPUT_FILE_PREFIX, DER2) : downloadFilename.replace(INPUT_FILE_PREFIX, SCT2);
+			LOGGER.info("Rename {} to {} for archiving", downloadFilename, rename);
+			final File localFile = new File(deltaTempDir, rename);
+			try (InputStream inputFileStream = productInputFileService.getFileInputStream(build.getProduct().getReleaseCenter().getBusinessKey()
+					, build.getProduct().getBusinessKey(), downloadFilename);
+			FileOutputStream out = new FileOutputStream(localFile)) {
+				if (inputFileStream != null) {
+					StreamUtils.copy(inputFileStream, out);
+				} else {
+					throw new ProcessingException("Didn't find input file:" + downloadFilename);
+				}
+			}
+		}
+		return deltaTempDir;
+	}
+
+	public File downloadOutputDelta(final Build build) throws ProcessingException, IOException {
+		LOGGER.info("Creating delta archive for classification with output files");
+		final File deltaTempDir = Files.createTempDir();
+		for (String fileName : buildDAO.listOutputFilePaths(build)) {
+			if (!isRequiredFileForClassification(fileName)) continue;
+			LOGGER.info("Prepare {} for archiving", fileName);
+			String rename = fileName.startsWith("x") ? fileName.substring(1) : fileName;
+			LOGGER.info("Rename {} to {} for archiving", fileName, rename);
+			final File localFile = new File(deltaTempDir, rename);
+			try (InputStream inputFileStream = buildDAO.getOutputFileInputStream(build, fileName);
+			FileOutputStream out = new FileOutputStream(localFile)) {
+				if (inputFileStream != null) {
+					StreamUtils.copy(inputFileStream, out);
+				} else {
+					throw new ProcessingException("Didn't find input file:" + fileName);
+				}
+			}
+		}
+		return deltaTempDir;
+	}
+
+	public String validateClassificationResults(File classificationResults, Build build) throws IOException, BusinessServiceException {
+		// Raise the errors if the results returned from Classification Service are not empty
+		StringBuilder errorMessageBuilder = new StringBuilder();
+		File classifierResult = new File(Files.createTempDir(), "result");
+		if (!classifierResult.exists()) {
+			classifierResult.mkdir();
+		}
+		if (classifierResult.exists() && classifierResult.isDirectory()) {
+			ZipFileUtils.extractFilesFromZipToOneFolder(classificationResults, classifierResult.getAbsolutePath());
+		} else {
+			throw new BusinessServiceException("Failed to create folder to extract classification results:" + classifierResult);
+		}
+		for (File file : classifierResult.listFiles()) {
+			if (file.getName().endsWith(TXT_FILE_EXTENSION)) {
+				if (file.getName().startsWith(RELASHIONSHIP_DELTA_PREFIX)) {
+					List<String> results = FileUtils.readLines(file);
+					if (results.size() > 1) {
+						String errorMessage = String.format(ERROR_MSG_FORMAT, "relationship", (results.size() - 1));
+						LOGGER.error(errorMessage);
+						buildDAO.putClassificationResultOutputFile(build, file);
+						errorMessageBuilder.append(errorMessage);
+					}
+				} else if(file.getName().startsWith(EQUIVALENT_CONCEPT_REFSET)) {
+					List<String> results = FileUtils.readLines(file);
+					if (results.size() > 1) {
+						String errorMessage = String.format(ERROR_MSG_FORMAT, "equivalent concept refsets", (results.size() - 1));
+						LOGGER.error(errorMessage);
+						buildDAO.putClassificationResultOutputFile(build, file);
+						errorMessageBuilder.append(errorMessage);
 					}
 				}
 
 			}
-			return localFilePaths;
 		}
-
-		private boolean isRequiredFileForClassification(String filename) {
-			return filename.matches(REL2_CONCEPT_PATTERN) || filename.matches(REL2_STATED_RELATIONSHIP_PATTERN) || filename.matches(REL2_RELATIONSHIP_PATTERN)
-					|| filename.contains(REL2_MRCM_ATTRIBUTE_DOMAIN_DELTA) || filename.matches(OWL_REFSET_FILE_PATTERN);
-		}
-
-
+		return errorMessageBuilder.toString();
 	}
+
+	private boolean isRequiredFileForClassification(String filename) {
+		return filename.matches(CONCEPT_PATTERN) || filename.matches(STATED_RELATIONSHIP_PATTERN) || filename.matches(RELATIONSHIP_PATTERN)
+				|| filename.contains(MRCM_ATTRIBUTE_DOMAIN_DELTA) || filename.matches(OWL_REFSET_FILE_PATTERN);
+	}
+
+}
--- data-service/src/main/java/org/ihtsdo/buildcloud/service/precondition/TermServerClassificationResultsCheck.java
+++ data-service/src/main/java/org/ihtsdo/buildcloud/service/precondition/TermServerClassificationResultsCheck.java
@@ -1,25 +1,17 @@
 package org.ihtsdo.buildcloud.service.precondition;
 
-import com.google.common.io.Files;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.ihtsdo.buildcloud.dao.BuildDAO;
 import org.ihtsdo.buildcloud.entity.Build;
 import org.ihtsdo.buildcloud.service.NetworkRequired;
 import org.ihtsdo.buildcloud.service.RF2ClassificationService;
-import org.ihtsdo.otf.rest.exception.BusinessServiceException;
-import org.ihtsdo.otf.utils.ZipFileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.List;
 
-import static org.ihtsdo.buildcloud.service.RF2ClassificationService.EQUIVALENT_CONCEPT_REFSET;
-import static org.ihtsdo.buildcloud.service.build.RF2Constants.RELASHIONSHIP_DELTA_PREFIX;
-import static org.ihtsdo.buildcloud.service.build.RF2Constants.TXT_FILE_EXTENSION;
 
 public class TermServerClassificationResultsCheck extends PreconditionCheck implements NetworkRequired {
 
@@ -29,26 +21,26 @@ public class TermServerClassificationResultsCheck extends PreconditionCheck impl
 	@Autowired
 	private BuildDAO buildDAO;
 
+	public static final String SKIP_MESSAGE = "Skipped TermServer Classification Results Check. Reason: This product is a derivative product";
 	private static final Logger LOGGER = LoggerFactory.getLogger(TermServerClassificationResultsCheck.class);
-
 
 	@Override
 	public void runCheck(Build build) {
 		boolean isDerivativeProduct = buildDAO.isDerivativeProduct(build);
-		LOGGER.info("Term Server Classification Results Check: isDerivativeProduct={}", isDerivativeProduct);
+		LOGGER.info("TermServer Classification Results Check: isDerivativeProduct={}", isDerivativeProduct);
 		if (isDerivativeProduct) {
-			StringBuilder reasonBuilder = new StringBuilder("Skipped Term Server Classification Results Check. Reason: ");
-			reasonBuilder.append("This product is a derivative product.");
-			LOGGER.info(reasonBuilder.toString());
-			notRun(reasonBuilder.toString());
+			LOGGER.info(SKIP_MESSAGE);
+			notRun(SKIP_MESSAGE);
 			return;
 		}
 		LOGGER.info("Run classification validation check for input files of build {}", build.getId());
+		File results = null;
+		File deltaFile = null;
 		try {
 			// Classify and validate the returned results
-			File classificationResults = rf2ClassificationService.classify(build);
-			File resultTempDir = Files.createTempDir();
-			String errorMessage = validateClassificationResults(classificationResults, resultTempDir, build);
+			deltaFile = rf2ClassificationService.downloadInputDelta(build);
+			results = rf2ClassificationService.classify(build, deltaFile);
+			String errorMessage = rf2ClassificationService.validateClassificationResults(results, build);
 			if (StringUtils.isNotBlank(errorMessage)) {
 				fatalError(errorMessage);
 				LOGGER.error("Classification validation check has failed");
@@ -57,46 +49,13 @@ public class TermServerClassificationResultsCheck extends PreconditionCheck impl
 				LOGGER.info("Classification validation check has passed");
 			}
 			LOGGER.info("Complete classification validation check for input files of build {}", build.getId());
-
 		} catch (Exception e) {
-			LOGGER.error("Error occurred during classification results validation due to: ", e);
-			fatalError("Error occurred during classification results validation due to: " + e.getMessage());
+			String errorMsg = "Error occurred during classification results validation due to: ";
+			LOGGER.error(errorMsg, e);
+			fatalError(errorMsg + e.getMessage());
+		} finally {
+			FileUtils.deleteQuietly(results);
+			FileUtils.deleteQuietly(deltaFile);
 		}
-	}
-
-	private String validateClassificationResults(File classificationResults, File resultTemp, Build build) throws IOException, BusinessServiceException {
-		// Raise the errors if the results returned from Classification Service are not empty
-		StringBuilder errorMessageBuilder = new StringBuilder();
-		File classifierResult = new File(resultTemp, "result");
-		if (!classifierResult.exists()) {
-			classifierResult.mkdir();
-		}
-		if (classifierResult.exists() && classifierResult.isDirectory()) {
-			ZipFileUtils.extractFilesFromZipToOneFolder(classificationResults, classifierResult.getAbsolutePath().toString());
-		} else {
-			throw new BusinessServiceException("Failed to create folder to extract classification results:" + classifierResult);
-		}
-		for (File file : classifierResult.listFiles()) {
-			if (file.getName().endsWith(TXT_FILE_EXTENSION)) {
-				if(file.getName().startsWith(RELASHIONSHIP_DELTA_PREFIX)) {
-					List<String> results = FileUtils.readLines(file);
-					if (results.size() > 1) {
-						String errorMessage = "Inconsistencies found in relationship file in classification results - expected 0 records but found " + (results.size() - 1) + " records from classification service. ";
-						LOGGER.error(errorMessage);
-						buildDAO.putClassificationResultOutputFile(build, file);
-						errorMessageBuilder.append(errorMessage);
-					}
-				} else if(file.getName().startsWith(EQUIVALENT_CONCEPT_REFSET)) {
-					List<String> results = FileUtils.readLines(file);
-					if (results.size() > 1) {
-						String errorMessage = "Inconsistencies found in equivalent concept refsets file in classification results - expected 0 records but found " + (results.size() - 1) + " records from classification service. ";
-						LOGGER.error(errorMessage);
-						buildDAO.putClassificationResultOutputFile(build, file);
-						errorMessageBuilder.append(errorMessage);
-					}
-				}
-			}
-		}
-		return errorMessageBuilder.toString();
 	}
 }
