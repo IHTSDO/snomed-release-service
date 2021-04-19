3b18d16b53aa009f5c17472807d271827a38bba2
Tyler Cooksey <Tyler-Cooksey>
2021-04-19 12:00:37 +0100

    FRI-21 - Updated Appender to use Log4j2 and not log4j.

--- telemetry/src/main/java/org/ihtsdo/telemetry/client/TelemetryEventAppender.java
+++ telemetry/src/main/java/org/ihtsdo/telemetry/client/TelemetryEventAppender.java
@@ -1,16 +1,23 @@
 package org.ihtsdo.telemetry.client;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.log4j.Layout;
 import org.apache.log4j.MDC;
-import org.apache.log4j.PatternLayout;
-import org.apache.log4j.WriterAppender;
-import org.apache.log4j.spi.LoggingEvent;
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.Core;
+import org.apache.logging.log4j.core.Filter;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.appender.AbstractAppender;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
+import org.apache.logging.log4j.core.config.plugins.PluginElement;
+import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+import org.apache.logging.log4j.core.layout.PatternLayout;
 import org.ihtsdo.telemetry.core.Constants;
 import org.ihtsdo.telemetry.core.JmsFactory;
 import org.ihtsdo.telemetry.core.TelemetryRuntimeException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 import javax.jms.JMSException;
 import javax.jms.MessageProducer;
@@ -19,48 +26,34 @@ import javax.jms.TextMessage;
 import java.util.Map;
 import java.util.UUID;
 
-public class TelemetryEventAppender extends WriterAppender {
+@Plugin(name = "TelemetryEventAppender", category = Core.CATEGORY_NAME, elementType = Appender.ELEMENT_TYPE)
+@Service
+public class TelemetryEventAppender extends AbstractAppender {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(TelemetryEventAppender.class);
 
 	private static final String PATTERN = "%d{yyyyMMdd_HH:mm:ss} %-5p %C.%M - %m%n";
+
 	private MessageProducer producer;
+
 	private Session session;
-	private Logger logger = LoggerFactory.getLogger(TelemetryEventAppender.class);
+
 	private String service;
-	private String environment = "LocalHost";
 
-	public TelemetryEventAppender() {
-		// Hardcoding pattern because should not be under host application control
-		super.setLayout(new PatternLayout(PATTERN));
-		logger.info("Telemetry Service: {}", service);
-		logger.info("Telemetry Environment: {}", environment);
+	private String environment;
+
+	public TelemetryEventAppender(final String name, final Filter filter, final String service, final String environment) {
+		super(name, filter, PatternLayout.newBuilder().withPattern(PATTERN).build());
+		this.service = service == null ? "SRS" : service;
+		this.environment = environment == null ? "LocalHost" : environment;
+		LOGGER.info("Telemetry Service: {}", service);
+		LOGGER.info("Telemetry Environment: {}", environment);
 	}
 
-	@Override
-	public void append(LoggingEvent event) {
-		if (producer == null) {
-			producer = createProducer();
-		}
-		if (producer != null) {
-			try {
-				TextMessage message;
-				if (MDC.get(Constants.START_STREAM) != null) {
-					message = createStartStreamMessage();
-				} else if (MDC.get(Constants.FINISH_STREAM) != null) {
-					message = createFinishStreamMessage();
-				} else {
-					message = createEventMessage(event, this.service, this.environment);
-				}
-				logger.debug("Sending message '{}'", message.getText());
-				producer.send(message);
-			} catch (JMSException e) {
-				throw new TelemetryRuntimeException("Failed to create event message.", e);
-			}
-		}
-	}
-
-	@Override
-	public void setLayout(Layout layout) {
-		logger.warn("Ignoring layout from log4j config.");
+	@PluginFactory
+	public static TelemetryEventAppender createAppender(@PluginAttribute("name") final String name,
+			@PluginElement("Filter") final Filter filter, @PluginAttribute("env") final String environment) {
+		return new TelemetryEventAppender(name, filter, "SRS", environment);
 	}
 
 	private TextMessage createStartStreamMessage() throws JMSException {
@@ -80,11 +73,11 @@ public class TelemetryEventAppender extends WriterAppender {
 		return message;
 	}
 
-	private TextMessage createEventMessage(LoggingEvent event, String service, String environment) throws JMSException {
-		TextMessage message = createMessage(this.layout.format(event));
+	private TextMessage createEventMessage(LogEvent event, String service, String environment) throws JMSException {
+		TextMessage message = createMessage(event.getMessage().getFormattedMessage());
 		message.setStringProperty(Constants.LEVEL, event.getLevel().toString());
-		message.setLongProperty(Constants.TIME_STAMP, event.getTimeStamp());
-		message.setStringProperty(Constants.EXCEPTION, StringUtils.join(event.getThrowableStrRep(), "\n"));
+		message.setLongProperty(Constants.TIME_STAMP, event.getNanoTime());
+		message.setStringProperty(Constants.EXCEPTION, StringUtils.join(event.getThrown().getMessage(), "\n"));
 		message.setStringProperty(Constants.SERVICE, service);
 		message.setStringProperty(Constants.ENVIRONMENT, environment);
 		@SuppressWarnings("unchecked")
@@ -112,7 +105,7 @@ public class TelemetryEventAppender extends WriterAppender {
 			session = new JmsFactory().createSession();
 			return session.createProducer(session.createQueue(Constants.QUEUE_RELEASE_EVENTS));
 		} catch (JMSException e) {
-			logger.warn("Can't connect to message broker.");
+			LOGGER.warn("Can't connect to message broker.");
 			return null;
 		}
 	}
@@ -135,4 +128,26 @@ public class TelemetryEventAppender extends WriterAppender {
 		}
 	}
 
+	@Override
+	public void append(LogEvent logEvent) {
+		if (producer == null) {
+			producer = createProducer();
+		}
+		if (producer != null) {
+			try {
+				TextMessage message;
+				if (MDC.get(Constants.START_STREAM) != null) {
+					message = createStartStreamMessage();
+				} else if (MDC.get(Constants.FINISH_STREAM) != null) {
+					message = createFinishStreamMessage();
+				} else {
+					message = createEventMessage(logEvent, this.service, this.environment);
+				}
+				LOGGER.debug("Sending message '{}'", message.getText());
+				producer.send(message);
+			} catch (JMSException e) {
+				throw new TelemetryRuntimeException("Failed to create event message.", e);
+			}
+		}
+	}
 }
