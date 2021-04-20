865bde9769a2a45b5971553347c322de8ba474eb
Tyler Cooksey <Tyler-Cooksey>
2021-04-20 17:16:18 +0100

    FRI-21 - Added extra logging for debugging precheck rvf issue

--- data-service/src/main/java/org/ihtsdo/buildcloud/service/precondition/PreconditionManager.java
+++ data-service/src/main/java/org/ihtsdo/buildcloud/service/precondition/PreconditionManager.java
@@ -33,12 +33,16 @@ public class PreconditionManager {
 	 */
 	public List<PreConditionCheckReport> runPreconditionChecks(final Build build) {
 		List<PreConditionCheckReport> checkReports = new ArrayList<>();
+		LOGGER.info("Local RVF Enabled: {}", localRvf);
 		for (PreconditionCheck thisCheck : preconditionChecks) {
+			LOGGER.info("Online mode: {}", onlineMode);
 			if (onlineMode || !NetworkRequired.class.isAssignableFrom(thisCheck.getClass())
 					|| (RF2FilesCheck.class.isAssignableFrom(thisCheck.getClass()) && localRvf)) {
+				LOGGER.info("Statement has been reached! - 1");
 				if (thisCheck instanceof TermServerClassificationResultsCheck && !build.getConfiguration().useClassifierPreConditionChecks() ) {
 					continue;
 				}
+				LOGGER.info("Statement has been reached! - 2");
 				thisCheck.runCheck(build);
 				checkReports.add(thisCheck.getReport());
 			} else {
@@ -58,6 +62,7 @@ public class PreconditionManager {
 	public void setPreconditionChecks(List<PreconditionCheck> preconditionChecks) {
 		this.preconditionChecks = preconditionChecks;
 	}
+
 
 	public void setOfflineMode(@Value("${offlineMode}") boolean offlineMode) {
 		this.onlineMode = !offlineMode;
