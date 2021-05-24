65141622d0c13f485856a2814e0215f07e0cc089
Tyler Cooksey <Tyler-Cooksey>
2021-05-24 16:39:07 +0100

    FRI-99 - Removed status to build object.

--- src/main/java/org/ihtsdo/buildcloud/service/buildstatuslistener/BuildStatusListenerService.java
+++ src/main/java/org/ihtsdo/buildcloud/service/buildstatuslistener/BuildStatusListenerService.java
@@ -110,7 +110,6 @@ public class BuildStatusListenerService {
 		final Build.Status buildStatus = getBuildStatusFromRVF(message, build, product);
 		if (buildStatus != null) {
 			build.setProduct(product);
-			build.setStatus(buildStatus);
 			final BuildReport buildReport = getBuildReportFile(build);
 			if (buildReport != null) {
 				build.setBuildReport(buildReport);
