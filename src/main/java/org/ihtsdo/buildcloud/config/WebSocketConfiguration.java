b698635bfc588e76f0d583c8c5aac199d70aa404
CoderMChu <mch@ihtsdo.org>
2021-07-28 15:43:55 +0100

    FRI-191 Disable WebSocketMessageBrokerStats logging on SRS worker

--- src/main/java/org/ihtsdo/buildcloud/config/WebSocketConfiguration.java
+++ src/main/java/org/ihtsdo/buildcloud/config/WebSocketConfiguration.java
@@ -1,11 +1,13 @@
 package org.ihtsdo.buildcloud.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@ConditionalOnProperty(name = "srs.manager", havingValue = "true")
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
