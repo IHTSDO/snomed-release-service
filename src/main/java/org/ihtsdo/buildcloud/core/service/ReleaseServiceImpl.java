f57f8f1c077b9bc080b21230dcf6a1e58e8ff2f9
Quyen Ly <quocquyenit@gmail.com>
2021-11-11 11:48:16 +0700

    FRI-148 Added new API to start new authoring cycle for daily build

--- src/main/java/org/ihtsdo/buildcloud/core/service/ReleaseServiceImpl.java
+++ src/main/java/org/ihtsdo/buildcloud/core/service/ReleaseServiceImpl.java
@@ -1,11 +1,17 @@
 package org.ihtsdo.buildcloud.core.service;
 
-import org.ihtsdo.buildcloud.core.entity.Build;
-import org.ihtsdo.buildcloud.core.entity.BuildConfiguration;
-import org.ihtsdo.buildcloud.core.entity.Product;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.ihtsdo.buildcloud.core.dao.BuildDAO;
 import org.ihtsdo.buildcloud.core.dao.InputFileDAO;
+import org.ihtsdo.buildcloud.core.entity.Build;
 import org.ihtsdo.buildcloud.core.entity.Build.Status;
+import org.ihtsdo.buildcloud.core.entity.BuildConfiguration;
+import org.ihtsdo.buildcloud.core.entity.Product;
+import org.ihtsdo.buildcloud.core.manifest.FolderType;
+import org.ihtsdo.buildcloud.core.manifest.ListingType;
+import org.ihtsdo.buildcloud.core.service.build.RF2Constants;
 import org.ihtsdo.buildcloud.core.service.inputfile.gather.InputGatherReport;
 import org.ihtsdo.buildcloud.core.service.inputfile.prepare.FileProcessingReportDetail;
 import org.ihtsdo.buildcloud.core.service.inputfile.prepare.ReportType;
@@ -22,8 +28,15 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
+import java.io.*;
+import java.text.ParseException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Service
@@ -41,6 +54,9 @@ public class ReleaseServiceImpl implements ReleaseService {
 
 	@Autowired
 	private InputFileService inputFileService;
+
+	@Autowired
+	private ProductService productService;
 
 	@Autowired
 	private BuildService buildService;
@@ -73,12 +89,102 @@ public class ReleaseServiceImpl implements ReleaseService {
 		}
 	}
 
+	@Override
+	public void startNewAuthoringCycle(String releaseCenterKey, String productKey, String effectiveTime, String productKeySource, String dependencyPackage) throws BusinessServiceException, ParseException, JAXBException, IOException {
+		Product product = productService.find(releaseCenterKey, productKey, false);
+		if (product == null) {
+			throw new BusinessServiceRuntimeException("Could not find product " + productKey + " in release center " + releaseCenterKey);
+		}
+		Product productSource = productService.find(releaseCenterKey, productKeySource, false);
+		if (productSource == null) {
+			throw new BusinessServiceRuntimeException("Could not find product " + productKeySource + " in release center " + releaseCenterKey);
+		}
+
+		List<Build> builds = buildService.findAllDesc(releaseCenterKey, productKeySource, false, false, false, null);
+		Build latestPublishedBuild = null;
+		for (int i = 0; i < builds.size(); i++) {
+			if (builds.get(i).getTags() != null && builds.get(i).getTags().contains(Build.Tag.PUBLISHED)) {
+				latestPublishedBuild = builds.get(i);
+				break;
+			}
+		}
+		if (latestPublishedBuild != null) {
+			latestPublishedBuild = buildService.find(releaseCenterKey, productKeySource, latestPublishedBuild.getId(), true, false, false, null);
+			BuildConfiguration configuration = latestPublishedBuild.getConfiguration();
+			if (configuration.getEffectiveTime().compareTo(DateFormatUtils.ISO_DATE_FORMAT.parse(effectiveTime)) >= 0) {
+				throw new BusinessServiceRuntimeException("The new effective time must be greater than the latest published effective time " + configuration.getEffectiveTimeFormatted());
+			}
+
+			String previousPackage = getPreviousPackage(latestPublishedBuild) + RF2Constants.ZIP_FILE_EXTENSION;
+			replaceManifestFile(releaseCenterKey, productKey, latestPublishedBuild, effectiveTime, configuration.getEffectiveTimeSnomedFormat());
+			inputFileService.copyExternallyMaintainedFiles(releaseCenterKey, configuration.getEffectiveTimeSnomedFormat(), effectiveTime.replaceAll("-", ""), false);
+			updateProduct(releaseCenterKey, productKey, effectiveTime, dependencyPackage, previousPackage);
+		} else {
+			LOGGER.info("The product {} does not have any build which has been published yet", productKeySource);
+		}
+	}
+
+	private void updateProduct(String releaseCenterKey, String productKey, String effectiveTime, String dependencyPackage, String previousPackage) throws BusinessServiceException {
+		Map<String, String> changes = new HashMap<>();
+		changes.put(ProductService.DAILY_BUILD, ProductService.TRUE);
+		changes.put(ProductService.EFFECTIVE_TIME, effectiveTime);
+		changes.put(ProductService.PREVIOUS_PUBLISHED_PACKAGE, previousPackage);
+		if (!StringUtils.isEmpty(dependencyPackage)) {
+			changes.put(ProductService.DEPENDENCY_RELEASE_PACKAGE, dependencyPackage);
+			changes.put(ProductService.EXTENSION_DEPENDENCY_RELEASE, dependencyPackage); // QA config
+		}
+		if ("international".equalsIgnoreCase(releaseCenterKey)) {
+			changes.put(ProductService.PREVIOUS_INTERNATIONAL_RELEASE, previousPackage); // QA config
+		} else {
+			changes.put(ProductService.PREVIOUS_EXTENSION_RELEASE, previousPackage); // QA config
+		}
+		productService.update(releaseCenterKey, productKey, changes);
+	}
+
+	private String getPreviousPackage(final Build build) throws JAXBException {
+		final InputStream manifestStream = buildDAO.getManifestStream(build);
+		final Unmarshaller unmarshaller = JAXBContext.newInstance(RF2Constants.MANIFEST_CONTEXT_PATH).createUnmarshaller();
+		final ListingType manifestListing = unmarshaller.unmarshal(new StreamSource(manifestStream), ListingType.class).getValue();
+		String latestPackageName = null;
+		if (manifestListing != null) {
+			final FolderType rootFolder = manifestListing.getFolder();
+			if (rootFolder != null) {
+				latestPackageName = rootFolder.getName();
+			}
+		}
+		if (latestPackageName == null) {
+			throw new BusinessServiceRuntimeException("Could not find package name from manifest.xml");
+		}
+		return latestPackageName;
+	}
+
+	private void replaceManifestFile(String releaseCenterKey, String productKey, Build build, String effectiveTime, String previousEffectiveTime) throws IOException {
+		File tmpFile = File.createTempFile("manifest", ".xml");
+		try {
+			final InputStream manifestStream = buildDAO.getManifestStream(build);
+			try (BufferedReader reader = new BufferedReader(new InputStreamReader(manifestStream));
+				 PrintWriter writer = new PrintWriter(new BufferedOutputStream(new FileOutputStream(tmpFile)));) {
+				String str;
+				while ((str = reader.readLine()) != null) {
+					str = str.replaceAll("PRODUCTION", "DAILYBUILD");
+					str = str.replaceAll("_" + previousEffectiveTime, "_" + effectiveTime.replaceAll("-", ""));
+					writer.println(str);
+				}
+			} catch (FileNotFoundException e) {
+				LOGGER.error(e.getMessage());
+			}
+			inputFileService.putManifestFile(releaseCenterKey, productKey, new FileInputStream(tmpFile), "manifest.xml", tmpFile.length());
+		} finally {
+			FileUtils.forceDelete(tmpFile);
+		}
+	}
+
 	private boolean prepareInputFiles(Build build, Product product) throws BusinessServiceException {
 		// Prepare input files from sources when available
 		SourceFileProcessingReport sourceFileProcessingReport = inputFileService.prepareInputFiles(product.getReleaseCenter().getBusinessKey(), product.getBusinessKey(), build, true);
 		if (sourceFileProcessingReport.getDetails().get(ReportType.ERROR) != null) {
 			LOGGER.error("Error occurred when processing input files");
-			List <FileProcessingReportDetail> errorDetails = sourceFileProcessingReport.getDetails().get(ReportType.ERROR);
+			List<FileProcessingReportDetail> errorDetails = sourceFileProcessingReport.getDetails().get(ReportType.ERROR);
 			for (FileProcessingReportDetail errorDetail : errorDetails) {
 				LOGGER.error("File: {} -> Error Details: {}", errorDetail.getFileName(), errorDetail.getMessage());
 			}
