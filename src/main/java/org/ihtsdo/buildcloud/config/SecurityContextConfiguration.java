3392009794cd2d21b5017cbd4b98a27c33c76adc
Elena Ilyukhina <eil@ihtsdo.org>
2022-04-04 18:20:55 +0100

    FRI-225 - Added basic authentication

--- src/main/java/org/ihtsdo/buildcloud/config/SecurityContextConfiguration.java
+++ src/main/java/org/ihtsdo/buildcloud/config/SecurityContextConfiguration.java
@@ -4,33 +4,38 @@ import org.ihtsdo.sso.integration.RequestHeaderAuthenticationDecorator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityContextConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Bean
-	public UserDetailsService userDetailsService(@Value("${srs.basicAuth.username}") final String username,
-											 @Value("${srs.basicAuth.password}") final String password, @Value("ROLE_USER") final String authority) {
-		return new InMemoryUserDetailsManager(Collections.singleton(new User(username, password,
-				Collections.singleton(new SimpleGrantedAuthority(authority)))));
-	}
+	@Value("${srs.basicAuth.username}")
+	private String username;
 
-	@Bean
-	public RequestHeaderAuthenticationDecorator authenticationDecorator() {
-		return new RequestHeaderAuthenticationDecorator();
+	@Value("${srs.basicAuth.password}")
+	private String password;
+
+	/*
+	This configuration is required for basic authentication to work with this version of Spring Security (3.2.5.RELEASE).
+	In later versions default user credentials are read from the following properties in the application.properties file:
+		spring.security.user.name
+		spring.security.user.password
+		spring.security.user.roles
+	This functionality is implemented in class InitializeUserDetailsBeanManagerConfigurer.java since 4.1.x
+    */
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+				.withUser(username)
+				.password(password)
+				.roles("USER");
 	}
 
 	@Override
@@ -40,12 +45,14 @@ public class SecurityContextConfiguration extends WebSecurityConfigurerAdapter {
 				.antMatchers("/swagger-ui.html",
 						"/swagger-resources/**",
 						"/v2/api-docs",
-						"/webjars/springfox-swagger-ui/**")
-				.permitAll();
+						"/webjars/springfox-swagger-ui/**").permitAll()
+				.anyRequest().authenticated()
+				.and().httpBasic();
 		http.csrf().disable();
-		http.addFilterAfter(authenticationDecorator(), BasicAuthenticationFilter.class);
+		http.addFilterAfter(new RequestHeaderAuthenticationDecorator(), BasicAuthenticationFilter.class);
 	}
 
+	// Does not seem to be used anywhere
 	@Bean
 	public Http403ForbiddenEntryPoint http403ForbiddenEntryPoint() {
 		return new Http403ForbiddenEntryPoint();
