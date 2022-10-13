bd6c9e4021ad10787f5f3f3daf4a912ee4ed9ff2
--replace-all <quocquyenit@gmail.com>
2022-10-13 14:31:16 +0700

    FRI-502 Updated logic to check duplicate JIRA tickets

--- src/main/java/org/ihtsdo/buildcloud/core/dao/RVFFailureJiraAssociationDAO.java
+++ src/main/java/org/ihtsdo/buildcloud/core/dao/RVFFailureJiraAssociationDAO.java
@@ -2,8 +2,11 @@ package org.ihtsdo.buildcloud.core.dao;
 
 import org.ihtsdo.buildcloud.core.entity.RVFFailureJiraAssociation;
 
+import java.util.Date;
 import java.util.List;
 
 public interface RVFFailureJiraAssociationDAO extends EntityDAO<RVFFailureJiraAssociation> {
 	List<RVFFailureJiraAssociation> findByBuildKey(String centerKey, String productKey, String buildKey);
+
+	List<RVFFailureJiraAssociation> findByEffectiveTime(String centerKey, String productKey, Date effectiveTime);
 }
