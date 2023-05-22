11f56000c3c35a7444a325f3f045edbb13076210
Quyen Ly <quocquyenit@gmail.com>
2023-05-22 19:37:30 +0700

    Revert "FRI-646 Broadcast a JMS Topic message after pulishing a release"
    This reverts commit 37e6e5690a56f2c72c76a5f21bfd1c0e26fe9591.

--- src/main/java/org/ihtsdo/buildcloud/core/service/PublishServiceImpl.java
+++ src/main/java/org/ihtsdo/buildcloud/core/service/PublishServiceImpl.java
@@ -1,11 +1,7 @@
 package org.ihtsdo.buildcloud.core.service;
 
-import com.amazonaws.services.s3.model.AmazonS3Exception;
-import com.amazonaws.services.s3.model.ListObjectsRequest;
-import com.amazonaws.services.s3.model.ObjectListing;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.s3.model.*;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.MDC;
@@ -23,10 +19,8 @@ import org.ihtsdo.buildcloud.core.service.identifier.client.SchemeIdType;
 import org.ihtsdo.otf.dao.s3.S3Client;
 import org.ihtsdo.otf.dao.s3.helper.FileHelper;
 import org.ihtsdo.otf.dao.s3.helper.S3ClientHelper;
-import org.ihtsdo.otf.jms.MessagingHelper;
 import org.ihtsdo.otf.rest.client.RestClientException;
 import org.ihtsdo.otf.rest.client.terminologyserver.pojo.CodeSystem;
-import org.ihtsdo.otf.rest.client.terminologyserver.pojo.CodeSystemVersion;
 import org.ihtsdo.otf.rest.exception.BadRequestException;
 import org.ihtsdo.otf.rest.exception.BusinessServiceException;
 import org.ihtsdo.otf.rest.exception.EntityAlreadyExistsException;
@@ -48,13 +42,11 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.util.StreamUtils;
 
-import javax.jms.JMSException;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
 import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
@@ -86,12 +78,6 @@ public class PublishServiceImpl implements PublishService {
 	@Value("${srs.storage.bucketName}")
 	private String storageBucketName;
 
-	@Value("${srs.jms.time-to-live-seconds}")
-	private int messageTimeToLiveSeconds;
-
-	@Value("${srs.jms.queue.prefix}.release-published.event")
-	private String releasePublishDestination;
-
 	@Autowired
 	private TermServerService termServerService;
 
@@ -114,12 +100,6 @@ public class PublishServiceImpl implements PublishService {
 	
 	@Autowired
 	private SchemaFactory schemaFactory;
-
-	@Autowired
-	private ExecutorService executorService;
-
-	@Autowired
-	private MessagingHelper messagingHelper;
 	
 	private static final int BATCH_SIZE = 5000;
 	
@@ -278,33 +258,8 @@ public class PublishServiceImpl implements PublishService {
 							.orElse(null);
 					if (codeSystem != null && build.getConfiguration().getBranchPath().startsWith(codeSystem.getBranchPath())) {
 						try {
-							final String effectiveTimeSnomedFormat = build.getConfiguration().getEffectiveTimeSnomedFormat();
 							LOGGER.info("Update the release package for Code System Version: {}, {}, {}", codeSystem.getShortName(), build.getConfiguration().getEffectiveTimeSnomedFormat(), releaseFileName);
-							termServerService.updateCodeSystemVersionPackage(codeSystem.getShortName(), effectiveTimeSnomedFormat, releaseFileName);
-
-							// Send notification via JMS
-							executorService.submit(() -> {
-								try {
-									Long importDate = null;
-									List<CodeSystemVersion> versions = termServerService.getCodeSystemVersions(codeSystem.getShortName(), false, false);
-									for (CodeSystemVersion version : versions) {
-										if (String.valueOf(version.getEffectiveDate()).equals(effectiveTimeSnomedFormat) && version.getImportDate() != null) {
-											importDate = version.getImportDate().getTime();
-											break;
-										}
-									}
-
-									if (importDate != null) {
-										Map payload = new HashMap();
-										payload.put("codeSystemShortName", codeSystem.getShortName());
-										payload.put("effectiveDate", effectiveTimeSnomedFormat);
-										payload.put("importDate", importDate);
-										messagingHelper.publish(releasePublishDestination, payload, null, messageTimeToLiveSeconds);
-									}
-								} catch (JsonProcessingException | JMSException e) {
-									LOGGER.error("Failed to send PUBLISHED status to {}", releasePublishDestination);
-								}
-							});
+							termServerService.updateCodeSystemVersionPackage(codeSystem.getShortName(), build.getConfiguration().getEffectiveTimeSnomedFormat(), releaseFileName);
 						} catch (Exception e) {
 							concurrentPublishingBuildStatus.put(getBuildUniqueKey(build), new ProcessingStatus(Status.COMPLETED.name(), "The build has been published successfully but failed to update Code System Version Package.  Error message: " + e.getMessage()));
 							throw new BusinessServiceException("Failed to update Code System Version Package", e);
