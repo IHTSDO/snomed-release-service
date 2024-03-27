f3bd97ea8c525dc6252fc32cdf7bd7d27692fc72
Darryn McGaw <dmcg@ihtsdo.org>
2024-03-27 13:08:51 +0000

    MAINT-2511 Add optional paging to releases endpoint

--- src/main/java/org/ihtsdo/buildcloud/rest/controller/ReleasePackageController.java
+++ src/main/java/org/ihtsdo/buildcloud/rest/controller/ReleasePackageController.java
@@ -7,10 +7,7 @@ import org.snomed.module.storage.ModuleStorageCoordinator;
 import org.snomed.module.storage.ModuleStorageCoordinatorException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -26,7 +23,22 @@ public class ReleasePackageController {
     @GetMapping(value = "/releases")
     @Operation(summary = "Returns the list all release packages")
     @ResponseBody
-    public Map<String, List<ModuleMetadata>> getAllReleases() throws ModuleStorageCoordinatorException.OperationFailedException, ModuleStorageCoordinatorException.ResourceNotFoundException, ModuleStorageCoordinatorException.InvalidArgumentsException {
-        return moduleStorageCoordinator.getAllReleases();
+    public Map<String, List<ModuleMetadata>> getAllReleases(
+            @RequestParam(value = "page", required = false) String page,
+            @RequestParam(value = "size", required = false) String size
+    ) throws ModuleStorageCoordinatorException.OperationFailedException, ModuleStorageCoordinatorException.ResourceNotFoundException, ModuleStorageCoordinatorException.InvalidArgumentsException {
+        return moduleStorageCoordinator.getAllReleases(asIntegerOrFallback(page, 1), asIntegerOrFallback(size, 6));
+    }
+
+    private Integer asIntegerOrFallback(String integer, Integer fallback) {
+        if (integer == null || integer.isEmpty()) {
+            return fallback;
+        }
+
+        try {
+            return Integer.parseInt(integer);
+        } catch (NumberFormatException e) {
+            return fallback;
+        }
     }
 }
