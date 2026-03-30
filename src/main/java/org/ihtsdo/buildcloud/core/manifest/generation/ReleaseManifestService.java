45f3b90874782c14c559c46da89dc00bebf272e9
Quyen Ly <quocquyenit@gmail.com>
2026-03-30 14:25:42 +0700

    RAP-104 Add @Transactional to method generateManifestXml

--- src/main/java/org/ihtsdo/buildcloud/core/manifest/generation/ReleaseManifestService.java
+++ src/main/java/org/ihtsdo/buildcloud/core/manifest/generation/ReleaseManifestService.java
@@ -23,6 +23,7 @@ import org.ihtsdo.otf.rest.exception.BusinessServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import java.util.*;
@@ -73,6 +74,7 @@ public class ReleaseManifestService {
         this.releaseCenterDAO = releaseCenterDAO;
     }
 
+    @Transactional(readOnly = true)
     public String generateManifestXml(ManifestConfig manifestConfig, String releaseCenterKey, String branchPath, String effectiveTime, boolean isDailyBuild, boolean betaRelease
     ) throws BusinessServiceException, RestClientException {
 
